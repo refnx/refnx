17c4863736598709ff134545995996a1ddba79e4
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2011-04-14 07:04:48 +0000

    added a gravity correction

--- pyplatypus/Qtransforms.py
+++ pyplatypus/Qtransforms.py
@@ -3,7 +3,7 @@ import numpy as np
 kMassNeutron = 1.6749286e-27
 kPlanck = 6.62606896e-34
 #(mm/us)
-kMP = kMassNeutron/kPlanck
+kPlanck_over_MN = kPlanck/kMassNeutron
 
 def to_q(omega, lamda):
     """
@@ -18,10 +18,10 @@ def tof_to_lambda(TOF, distance):
     TOF = us
     return Angstom
     """
-    return kMP * TOF / distance * 1.e7
+    return kPlanck_over_MN * TOF / distance * 1.e7
     
 def lambda_to_tof(lamda, distance):	
-	return  1.e-7 * lamda * distance / kMP
+	return  1.e-7 * lamda * distance / kPlanck_over_MN
     
 def to_qzqy(omega, twotheta, lamda):
     """
--- pyplatypus/reduce.py
+++ pyplatypus/reduce.py
@@ -28,7 +28,7 @@ def reduce_stitch_files(reflect_list, direct_list, scalefactor = 1., collect = F
         #first process all the reflected beam spectra
         reflected_runs = [pro_nex.processnexusfile(file_number, basedir = basedir, rebinpercent = rebinpercent,                                                       background = background) for file_number in reflect_list if 0 < int(file_number) < 9999999]
         #now all the direct beam spectra
-        direct_runs = [pro_nex.processnexusfile(file_number, basedir = basedir, rebinpercent = rebinpercent, background = background) for file_number in direct_list if 0 < int(file_number) < 9999999]
+        direct_runs = [pro_nex.processnexusfile(file_number, basedir = basedir, rebinpercent = rebinpercent, background = background, isdirect = True) for file_number in direct_list if 0 < int(file_number) < 9999999]
             
         if collect:
             tempdir = tempfile.mkdtemp()
@@ -58,7 +58,6 @@ def reduce_stitch_files(reflect_list, direct_list, scalefactor = 1., collect = F
                                     val['M_lambda'],
                                      val['M_lambdaSD'],
                                       val['title'])
-        
        
         #now reduce all the files.
         zipped = zip(reflected_runs, direct_runs)
@@ -132,11 +131,8 @@ def reduce_single_file(reflect_beam, direct_beam, scalefactor = 1):
     M_twotheta = np.zeros(M_topandtail.shape, dtype = 'float64')
     
     if mode == 'FOC' or mode == 'POL' or mode == 'POLANAL' or mode == 'MT':
-        # (n,t)            (n,t)                            (n, )           
         omega = reflect_beam['M_beampos'] + np.reshape(reflect_beam['detectorZ'], (numspectra, 1))
-        #            (1, t)                     (1, )
         omega -= direct_beam['M_beampos'] + direct_beam['detectorZ']
-        #                (n, ) 
         omega /= np.reshape(reflect_beam['detectorY'], (numspectra, 1))
 
         omega = np.arctan(omega) / 2
@@ -145,14 +141,9 @@ def reduce_single_file(reflect_beam, direct_beam, scalefactor = 1):
         #print direct_beam['M_beampos'], direct_beam['detectorZ']
         #print omega * 180/np.pi
         
-        #(n, t, y)                    (n, )                                                      
         M_twotheta += np.reshape(reflect_beam['detectorZ'], (numspectra, 1, 1))
-        #             (1, 1, y)
         M_twotheta += np.reshape(np.arange(numypixels * 1.), (1, 1, numypixels)) * pro_nex.Y_PIXEL_SPACING
-        #                (1, t)                                                        (1, )
-        M_twotheta -= np.reshape(direct_beam['M_beampos'], (1, numtpixels, 1)) + direct_beam['detectorZ']
-        
-        #(n, t, y)                        (n,)
+        M_twotheta -= np.reshape(direct_beam['M_beampos'], (1, numtpixels, 1)) + direct_beam['detectorZ']        
         M_twotheta /= np.reshape(reflect_beam['detectorY'], (numspectra, 1, 1))
         M_twotheta = np.arctan(M_twotheta)
 
@@ -174,8 +165,9 @@ def reduce_single_file(reflect_beam, direct_beam, scalefactor = 1):
         M_twotheta = np.arctan(M_twotheta)
         M_twotheta += np.reshape(omega, (numspectra, numtpixels, 1))
    
-#    TODO workout corrected angle of incidence and input into offspecular calcn
+#    workout corrected angle of incidence and input into offspecular calcn
     M_omega = M_twotheta / 2
+#    print "angle of inc:", omega[0]
     
     #now normalise the counts in the reflected beam by the direct beam spectrum
     #this gives a reflectivity
@@ -183,8 +175,7 @@ def reduce_single_file(reflect_beam, direct_beam, scalefactor = 1):
     #this step probably produces negative reflectivities, or NaN if M_specD is 0.
     #ALSO, 
     #M_refSD has the potential to be NaN is M_topandtail or M_spec is 0.
-    
-    #        (n, t, y)        (1, t)    
+      
     M_ref, M_refSD = EP.EPdiv(M_topandtail,
                                 reflect_beam['M_topandtailSD'],
                                  np.reshape(direct_beam['M_spec'], (1, numtpixels, 1)),
@@ -195,17 +186,21 @@ def reduce_single_file(reflect_beam, direct_beam, scalefactor = 1):
     M_refSD = np.where(np.isinf(M_refSD), 0, M_refSD)
     
     #now calculate the Q values for the detector pixels.  Each pixel has different 2theta and different wavelength, ASSUME that they have the same angle of incidence
-    #(n, t, y)
-    #TODO workout gravity corrected angle of incidence and input into offspecular calcn
-    M_qz, M_qy = qtrans.to_qzqy(np.reshape(omega, (numspectra, numtpixels, 1)),
-                                 M_twotheta,
-                                  np.reshape(reflect_beam['M_lambda'], (numspectra, numtpixels, 1)))
+    M_qz = np.empty_like(M_twotheta)
+    M_qy = np.empty_like(M_twotheta)
+    
+    M_qz[:] = 2 * np.pi / reflect_beam['M_lambda'].reshape(numspectra, numtpixels, 1)
+    M_qz *= np.sin(M_twotheta - omega.reshape(numspectra, numtpixels, 1)) + np.sin(M_omega)
+    M_qy[:] = 2 * np.pi / reflect_beam['M_lambda'].reshape(numspectra, numtpixels, 1)
+    M_qy *= np.cos(M_twotheta - omega.reshape(numspectra, numtpixels, 1)) - np.cos(M_omega)
+
+#    M_qz, M_qy = qtrans.to_qzqy(np.reshape(omega, (numspectra, numtpixels, 1)),
+#                                 M_twotheta,
+#                                  np.reshape(reflect_beam['M_lambda'], (numspectra, numtpixels, 1)))
     
     #now calculate the full uncertainty in Q for each Q pixel
-    #(n, t, y)       (n, t)                        (n, t)
     M_qzSD = np.zeros_like(M_qz)
     M_qzSD += np.reshape((reflect_beam['M_lambdaSD'] / reflect_beam['M_lambda'])**2, (numspectra, numtpixels, 1))
-    #                               (n,)                                (n, t, y)
     M_qzSD += (np.reshape(reflect_beam['domega'], (numspectra, 1, 1)) / M_omega)**2
     M_qzSD = np.sqrt(M_qzSD)
     M_qzSD *= M_qz
