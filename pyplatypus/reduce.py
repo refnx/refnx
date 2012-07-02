0f3375e7ea60c19d001cf9556f7edd9305da8f6b
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2012-07-02 05:42:37 +0000

    more work

--- pyplatypus/reduce.py
+++ pyplatypus/reduce.py
@@ -1,243 +1,187 @@
 from __future__ import division
 import numpy as np
-import processnexus as pro_nex
+import processnexus as pn
 import ErrorProp as EP
 import Qtransforms as qtrans
 import string
-import reflect_dataset as rd
-import os
-import tempfile
-import shutil
-import zipfile
-import hashlib
-import quickplot
-import sys
+from time import gmtime, strftime
+import reflectDataset as rd
+    
+class reduce(object):
+	def __init__(self, reflect_beam_number, direct_beam_number, **kwds):
+		kwds['isdirect'] = False
+		self.reflect_beam = pn.processnexus(reflect_beam_number, **kwds)
+		self.reflect_beam.process(**kwds)
+		self.datafilenumber = self.reflect_beam.datafilenumber
+		
+		kwds['wavelengthbins']=	self.reflect_beam.M_lambdaHIST
+		kwds['isdirect'] = True
+		self.direct_beam = pn.processnexus(direct_beam_number, **kwds)
+		self.direct_beam.process(**kwds)
+		self.__reduce_single_angle()
+		
+	def get1Ddata(self, scanpoint = 0):
+		return (self.W_q[scanpoint], self.W_ref[scanpoint], self.W_refSD[scanpoint], self.W_qSD[scanpoint])
 
-def sanitize_string_input(file_list_string):
-    """
-    given a string like '1 2 3 4 1000 -1 sijsiojsoij' return an integer list where the numbers are greater than 0 and less than 9999999
-    it strips the string.ascii_letters and any string.punctuation, and converts all the numbers to ints.
-    """
-    return [int(x) for x in file_list_string.translate(None, string.punctuation).translate(None, string.ascii_letters).split() if 0 < int(x) < 9999999]
-    
-def reduce_stitch_files(reflect_list, direct_list, **kwds):
-    """
-    kwds passed onto processnexusfile
-    """
-    scalefactor = kwds.get('scalefactor', 1.)
-    collect = kwds.get('collect', False)
-    rebinpercent = kwds.get('rebinpercent', 4.)
-            
-    cwd = os.getcwd()
-    filename = ''
-    tempdir = ''
-    
-    try:
-        #first process all the reflected beam spectra
-        reflected_runs = [pro_nex.processnexusfile(file_number, **kwds) for file_number in reflect_list if 0 < int(file_number) < 9999999]
-        #now all the direct beam spectra
-        direct_runs = [pro_nex.processnexusfile(file_number, **kwds) for file_number in direct_list if 0 < int(file_number) < 9999999] 
-            
-        if collect:
-            tempdir = tempfile.mkdtemp()
-            masterdir = os.path.join(tempdir, 'master')
-            spectrumdir = os.path.join(tempdir, 'master', 'spectrum')
-            reflectdir = os.path.join(tempdir, 'master', 'reflect')
-            stitchdir = os.path.join(reflectdir, 'stitch')
-            os.mkdir(masterdir)
-            os.mkdir(spectrumdir)
-            os.mkdir(reflectdir)
-            os.mkdir(stitchdir)
-            os.chdir(spectrumdir)
-        
-        #write out all the spectra
-        for index, val in enumerate(reflected_runs):
-            pro_nex.spectrum_XML(val['runnumber'],
-                                  val['M_spec'],
-                                    val['M_specSD'],
-                                        val['M_lambda'],
-                                            val['M_lambdaSD'],
-                                                val['title'])
-            
-        for index, val in enumerate(direct_runs):
-            pro_nex.spectrum_XML(val['runnumber'],
-                                  val['M_spec'],
-                                   val['M_specSD'],
-                                    val['M_lambda'],
-                                     val['M_lambdaSD'],
-                                      val['title'])
-       
-        #now reduce all the files.
-        zipped = zip(reflected_runs, direct_runs)
-        ref_runs, direct_runs = zip(*zipped)
-        reduced_data = map(reduce_single_file, ref_runs, direct_runs)
-        
-        if collect:
-            os.chdir(reflectdir)
-        #write them all to file and create the stitched file
-        for index, val in enumerate(reduced_data):
-            #write the specular datasets
-            dataset = rd.Reflect_Dataset(val.get1Ddata(), Reduced_Output = val)
-            dataset.scale(scalefactor = scalefactor)
-            dataset.write_xml()
-            dataset.write_dat()
-            #write the offspecular datasets
-            dataset_2D = rd.Reflect_Dataset_2D(val.M_qz, val.M_qy, val.M_ref, val.M_refSD, val.reflect_beam_spectrum['runnumber'])
-            dataset_2D.write_xml()
-            #stitch the individual runs together
-            if index:
-                stitchedData.add_dataset(val)
-            else:
-                stitchedData = rd.Reflect_Dataset(val.get1Ddata(), Reduced_Output = val)
-                stitchedData.scale(scalefactor = scalefactor)
-            
-        #now get the stitched file
-        if len(reduced_data) > 1:
-            if collect:
-                os.chdir(stitchdir)
-            
-            stitchedData.rebin(rebinpercent = rebinpercent)
-            stitchedData.write_xml()
-            stitchedData.write_dat()
-            #print out a picture
-            W_q, W_ref, W_refSD, W_qSD = stitchedData.getData()
-            name = 'c_PLP{:07d}.png'.format(stitchedData._rnumber[0])
-            quickplot.the_ref_plot(name, W_q[0], W_ref[0], W_refSD[0])
-        
-        if collect:
-            os.chdir(tempdir)
-            shutil.make_archive('data','zip', masterdir)
+	def get2Ddata(self, scanpoint = 0):
+		return (self.M_qz[scanpoint], self.M_qy[scanpoint], self.M_ref[scanpoint], self.M_refSD[scanpoint])
+		
+	def scale(self, scale):
+		self.M_ref /= scale
+		self.M_refSD /=scale
+		self.W_ref /=scale
+		self.W_refSD /= scale
+		
+	def getReflectedDataset(self, scanpoint = 0):
+		reflectedDatasetObj = rd.reflectDataset()
+		reflectedDatasetObj.add_dataset(self, scanpoint = scanpoint)
+		return reflectedDatasetObj
+	
+	def writeOffspecular(self, f, scanpoint = 0):
+		__template_ref_xml = """<?xml version="1.0"?>
+		<REFroot xmlns="">
+		<REFentry time="$time">
+		<Title>$title</Title>
+		<User>$user</User>
+		<REFsample>
+		<ID>$sample</ID>
+		</REFsample>
+		<REFdata axes="Qz:Qy" rank="2" type="POINT" spin="UNPOLARISED" dim="$_numpointsz:$_numpointsy">
+		<Run filename="$_rnumber" preset="" size="">
+		</Run>
+		<R uncertainty="dR">$_r</R>
+		<Qz uncertainty="dQz" units="1/A">$_qz</Qz>
+		<dR type="SD">$_dr</dR>
+		<Qy type="FWHM" units="1/A">$_qy</Qy>
+		</REFdata>
+		</REFentry>
+		</REFroot>"""
+		d = dict()
+		d['time'] = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+		d['_rnumber'] = self.reflect_beam.datafilenumber
+		d['_numpointsz'] = np.size(self.M_ref, 1)
+		d['_numpointsy'] = np.size(self.M_ref, 2) 
+		
+		s = string.Template(__template_ref_xml)
 
-            m = hashlib.md5()       
-            with open(os.path.join(tempdir,'data.zip'), 'r') as f:
-                for stuff in f.read():
-                    m.update(stuff)
+#			filename = 'off_PLP{:07d}_{:d}.xml'.format(self._rnumber, index)
+		d['_r'] = string.translate(repr(self.M_ref[scanpoint].tolist()), None, ',[]')
+		d['_qz'] = string.translate(repr(self.M_qz[scanpoint].tolist()), None, ',[]')
+		d['_dr'] = string.translate(repr(self.M_refSD[scanpoint].tolist()), None, ',[]')
+		d['_qy'] = string.translate(repr(self.M_qy[scanpoint].tolist()), None, ',[]')
+		
+		f = open(filename, 'w')
+		thefile = s.safe_substitute(d)
+		f.write(thefile)
+		f.truncate()
+			
+	def __reduce_single_angle(self):
+		numspectra = self.reflect_beam.numspectra
+		numtpixels = np.size(self.reflect_beam.M_topandtail, 1)
+		numypixels = np.size(self.reflect_beam.M_topandtail, 2)
+		
+		#calculate omega and two_theta depending on the mode.
+		mode = self.reflect_beam.mode
+		M_twotheta = np.zeros(self.reflect_beam.M_topandtail.shape, dtype = 'float64')
+		
+		if mode == 'FOC' or mode == 'POL' or mode == 'POLANAL' or mode == 'MT':
+			omega = self.reflect_beam.M_beampos + self.reflect_beam.detectorZ[:, np.newaxis]
+			omega -= self.direct_beam.M_beampos + self.direct_beam.detectorZ
+			omega /= self.reflect_beam.detectorY[:, np.newaxis]
 
-            filename = m.hexdigest() + '.zip'
-            shutil.copyfile(os.path.join(tempdir,'data.zip'), os.path.join(cwd, filename))
-            
-    except IOError as io:
-        sys.stderr.write(repr(io))
-    except Exception as e:
-        sys.stderr.write(repr(e))
-  
-    os.chdir(cwd)
-    shutil.rmtree(tempdir, True)
+			omega = np.arctan(omega) / 2
+			#print reflect_beam['M_beampos'], reflect_beam['detectorZ']
+			#print reflect_beam['detectorY']
+			#print direct_beam['M_beampos'], direct_beam['detectorZ']
+			#print omega * 180/np.pi
+			
+			M_twotheta += self.reflect_beam.detectorZ[:, np.newaxis, np.newaxis]
+			M_twotheta += np.arange(numypixels * 1.)[np.newaxis, np.newaxis, :] * pn.Y_PIXEL_SPACING
+			
+			M_twotheta -= self.direct_beam.M_beampos[:, :, np.newaxis] + self.direct_beam.detectorZ
+			M_twotheta /= self.reflect_beam.detectorY[:, np.newaxis, np.newaxis]
+			M_twotheta = np.arctan(M_twotheta)
 
-    return filename
-    
-def reduce_single_file(reflect_beam, direct_beam, scalefactor = 1):
-    #num files to output.
-    M_topandtail = reflect_beam['M_topandtail']
-    numspectra = len(reflect_beam['M_spec'])
-    numtpixels = np.size(M_topandtail, 1)
-    numypixels = np.size(M_topandtail, 2)
-    
-    #calculate omega and two_theta depending on the mode.
-    mode = reflect_beam['mode'][0]
-    M_twotheta = np.zeros(M_topandtail.shape, dtype = 'float64')
-    
-    if mode == 'FOC' or mode == 'POL' or mode == 'POLANAL' or mode == 'MT':
-        omega = reflect_beam['M_beampos'] + reflect_beam['detectorZ'][:, np.newaxis]
-        omega -= direct_beam['M_beampos'] + direct_beam['detectorZ']
-        omega /= reflect_beam['detectorY'][:, np.newaxis]
+			if omega[0,0] < 0:
+				omega = 0 - omega
+				M_twotheta = 0 - M_twotheta
+		elif mode == 'SB' or mode == 'DB':
+			omega = self.reflect_beam.M_beampos + self.reflect_beam.detectorZ[:, np.newaxis]
+			omega -= self.direct_beam.M_beampos + self.direct_beam.detectorZ
+			omega /= 2 * self.reflect_beam.detectorY[:, np.newaxis, np.newaxis]
+			omega = np.arctan(omega)   
+			
+			M_twotheta += np.arange(numypixels * 1.)[np.newaxis, np.newaxis, :] * pn.Y_PIXEL_SPACING
+			M_twotheta += self.reflect_beam.detectorZ[:, np.newaxis, np.newaxis]
+			M_twotheta -= self.direct_beam.M_beampos[:, :, np.newaxis] + self.direct_beam.detectorZ
+			M_twotheta -= self.reflect_beam.detectorY[:, np.newaxis, np.newaxis] * np.tan(omega[:, :, np.newaxis])
+			
+			M_twotheta /= self.reflect_beam.detectorY[:, np.newaxis, np.newaxis]
+			M_twotheta = np.arctan(M_twotheta)
+			M_twotheta += omega[:, :, np.newaxis]
+	   
+	#    workout corrected angle of incidence and input into offspecular calcn
+		M_omega = M_twotheta / 2
+	#    print "angle of inc:", omega[0]
+		
+		#now normalise the counts in the reflected beam by the direct beam spectrum
+		#this gives a reflectivity
+		#and propagate the errors, leaving the fractional variance (dr/r)^2
+		#this step probably produces negative reflectivities, or NaN if M_specD is 0.
+		#ALSO, 
+		#M_refSD has the potential to be NaN is M_topandtail or M_spec is 0.
 
-        omega = np.arctan(omega) / 2
-        #print reflect_beam['M_beampos'], reflect_beam['detectorZ']
-        #print reflect_beam['detectorY']
-        #print direct_beam['M_beampos'], direct_beam['detectorZ']
-        #print omega * 180/np.pi
-        
-        M_twotheta += reflect_beam['detectorZ'][:, np.newaxis, np.newaxis]
-        M_twotheta += np.arange(numypixels * 1.)[np.newaxis, np.newaxis, :] * pro_nex.Y_PIXEL_SPACING
-        M_twotheta -= direct_beam['M_beampos'][np.newaxis, :, np.newaxis] + direct_beam['detectorZ']        
-        M_twotheta /= reflect_beam['detectorY'][:, np.newaxis, np.newaxis]
-        M_twotheta = np.arctan(M_twotheta)
+		
+		M_ref, M_refSD = EP.EPdiv(self.reflect_beam.M_topandtail,
+									self.reflect_beam.M_topandtailSD,
+									 self.direct_beam.M_spec[:, :, np.newaxis],
+									  self.direct_beam.M_specSD[:, :, np.newaxis])
 
-        if omega[0,0] < 0:
-            omega = 0 - omega
-            M_twotheta = 0 - M_twotheta
-    elif mode == 'SB' or mode == 'DB':
-        omega = reflect_beam['M_beampos'] + reflect_beam['detectorZ'][:, np.newaxis]
-        omega -= direct_beam['M_beampos'] + direct_beam['detectorZ']
-        omega /= 2 * reflect_beam['detectorY'][:, np.newaxis, np.newaxis]
-        omega = np.arctan(omega)   
-        
-        M_twotheta += np.arange(numypixels * 1.)[np.newaxis, np.newaxis, :] * pro_nex.Y_PIXEL_SPACING
-        M_twotheta += reflect_beam['detectorZ'][:, np.newaxis, np.newaxis]
-        M_twotheta -= direct_beam['M_beampos'][np.newaxis, :, np.newaxis] + direct_beam['detectorZ']
-        M_twotheta -= reflect_beam['detectorY'][:, np.newaxis, np.newaxis] * np.tan(omega[:, :, np.newaxis])
-        
-        M_twotheta /= reflect_beam['detectorY'][:, np.newaxis, np.newaxis]
-        M_twotheta = np.arctan(M_twotheta)
-        M_twotheta += omega[:, :, np.newaxis]
-   
-#    workout corrected angle of incidence and input into offspecular calcn
-    M_omega = M_twotheta / 2
-#    print "angle of inc:", omega[0]
-    
-    #now normalise the counts in the reflected beam by the direct beam spectrum
-    #this gives a reflectivity
-    #and propagate the errors, leaving the fractional variance (dr/r)^2
-    #this step probably produces negative reflectivities, or NaN if M_specD is 0.
-    #ALSO, 
-    #M_refSD has the potential to be NaN is M_topandtail or M_spec is 0.
-      
-    M_ref, M_refSD = EP.EPdiv(M_topandtail,
-                                reflect_beam['M_topandtailSD'],
-                                 direct_beam['M_spec'][np.newaxis, :, np.newaxis],
-                                  direct_beam['M_specSD'][np.newaxis, :, np.newaxis])
+		#you may have had divide by zero's.
+		M_ref = np.where(np.isinf(M_ref), 0, M_ref)
+		M_refSD = np.where(np.isinf(M_refSD), 0, M_refSD)
+		
+		#now calculate the Q values for the detector pixels.  Each pixel has different 2theta and different wavelength, ASSUME that they have the same angle of incidence
+		M_qz = np.empty_like(M_twotheta)
+		M_qy = np.empty_like(M_twotheta)
+		
+		M_qz[:] = 2 * np.pi / self.reflect_beam.M_lambda[:, :, np.newaxis]
+		M_qz *= np.sin(M_twotheta - omega[:, :, np.newaxis]) + np.sin(M_omega)
+		M_qy[:] = 2 * np.pi / self.reflect_beam.M_lambda[:, :, np.newaxis]
+		M_qy *= np.cos(M_twotheta - omega[:, :, np.newaxis]) - np.cos(M_omega)
 
-    #you may have had divide by zero's.
-    M_ref = np.where(np.isinf(M_ref), 0, M_ref)
-    M_refSD = np.where(np.isinf(M_refSD), 0, M_refSD)
-    
-    #now calculate the Q values for the detector pixels.  Each pixel has different 2theta and different wavelength, ASSUME that they have the same angle of incidence
-    M_qz = np.empty_like(M_twotheta)
-    M_qy = np.empty_like(M_twotheta)
-    
-    M_qz[:] = 2 * np.pi / reflect_beam['M_lambda'][:, :, np.newaxis]
-    M_qz *= np.sin(M_twotheta - omega[:, :, np.newaxis]) + np.sin(M_omega)
-    M_qy[:] = 2 * np.pi / reflect_beam['M_lambda'][:, :, np.newaxis]
-    M_qy *= np.cos(M_twotheta - omega[:, :, np.newaxis]) - np.cos(M_omega)
-
-#    M_qz, M_qy = qtrans.to_qzqy(np.reshape(omega, (numspectra, numtpixels, 1)),
-#                                 M_twotheta,
-#                                  np.reshape(reflect_beam['M_lambda'], (numspectra, numtpixels, 1)))
-    
-    #now calculate the full uncertainty in Q for each Q pixel
-    M_qzSD = np.zeros_like(M_qz)
-    M_qzSD += np.reshape((reflect_beam['M_lambdaSD'] / reflect_beam['M_lambda'])**2, (numspectra, numtpixels, 1))
-    M_qzSD += (reflect_beam['domega'][:, np.newaxis, np.newaxis] / M_omega)**2
-    M_qzSD = np.sqrt(M_qzSD)
-    M_qzSD *= M_qz
-    
-    #scale reflectivity by scale factor
-    M_ref, M_refSD = EP.EPdiv(M_ref, M_refSD, scalefactor, 0)
-    
-    #now calculate the 1D output
-    W_q = qtrans.to_q(omega, reflect_beam['M_lambda'])
-    W_qSD = (reflect_beam['M_lambdaSD'] / reflect_beam['M_lambda'])**2
-    W_qSD += (reflect_beam['domega'][:, np.newaxis] / omega) ** 2
-    W_qSD = np.sqrt(W_qSD) * W_q
-    
-    lopx, hipx = reflect_beam['lopx'], reflect_beam['hipx']
-    
-    W_ref = np.sum(M_ref[:, :, lopx:hipx + 1], axis = 2)
-    W_refSD = np.sum(np.power(M_refSD[:, :, lopx:hipx + 1], 2), axis = 2)
-    W_refSD = np.sqrt(W_refSD)
-    
-    output = { 'W_q' : W_q,
-               'W_qSD' : W_qSD,
-               'W_ref' : W_ref,
-               'W_refSD' : W_refSD,
-               'M_ref' : M_ref,
-               'M_refSD' : M_refSD,
-               'M_qz' : M_qz,
-               'M_qzSD' : M_qzSD,
-               'M_qy' : M_qy,
-               'reflect_beam_spectrum' : reflect_beam,
-               'direct_beam_spectrum' : direct_beam}
-    obj = rd.Reduced_Output(output)    
-    return obj 
-    
+	#    M_qz, M_qy = qtrans.to_qzqy(np.reshape(omega, (numspectra, numtpixels, 1)),
+	#                                 M_twotheta,
+	#                                  np.reshape(reflect_beam['M_lambda'], (numspectra, numtpixels, 1)))
+		
+		#now calculate the full uncertainty in Q for each Q pixel
+		M_qzSD = np.zeros_like(M_qz)
+		M_qzSD += np.reshape((self.reflect_beam.M_lambdaSD / self.reflect_beam.M_lambda)**2, (numspectra, numtpixels, 1))
+		M_qzSD += (self.reflect_beam.domega[:, np.newaxis, np.newaxis] / M_omega)**2
+		M_qzSD = np.sqrt(M_qzSD)
+		M_qzSD *= M_qz
+		
+		#scale reflectivity by scale factor
+		#M_ref, M_refSD = EP.EPdiv(M_ref, M_refSD, scalefactor, 0)
+		
+		#now calculate the 1D output
+		W_q = qtrans.to_q(omega, self.reflect_beam.M_lambda)
+		W_qSD = (self.reflect_beam.M_lambdaSD / self.reflect_beam.M_lambda)**2
+		W_qSD += (self.reflect_beam.domega[:, np.newaxis] / omega) ** 2
+		W_qSD = np.sqrt(W_qSD) * W_q
+		
+		lopx, hipx = self.reflect_beam.lopx, self.reflect_beam.hipx
+		
+		W_ref = np.sum(M_ref[:, :, lopx:hipx + 1], axis = 2)
+		W_refSD = np.sum(np.power(M_refSD[:, :, lopx:hipx + 1], 2), axis = 2)
+		W_refSD = np.sqrt(W_refSD)
+		
+		self.W_q = W_q
+		self.W_qSD = W_qSD
+		self.W_ref = W_ref
+		self.W_refSD = W_refSD
+		self.M_ref = M_ref
+		self.M_refSD = M_refSD
+		self.M_qz = M_qz
+		self.M_qy = M_qy
+ 
