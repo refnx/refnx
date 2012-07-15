a39ac235b65c329cb0b6be99bedd14ef039071bc
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2012-07-15 10:40:24 +0000

    ref calc

--- pyplatypus/reflect.py
+++ pyplatypus/reflect.py
@@ -9,21 +9,29 @@ def reflectivity(coefs, qvals):
 		
 	nlayers = int(coefs[0])
 	
-	pj = np.array((len(qvals), nlayers + 2), dtype = 'complex')
+	pj = np.zeros((len(qvals), nlayers + 2), dtype = 'complex')
+	roughnesses = np.zeros(nlayers + 1, 'float')
+#	rj = np.array((len(qvals), nlayers + 1), 'complex')
 	
 	SLDfronting = np.complex(coefs[2] * 1.e-6 + coefs[3] * j)
 	SLDbacking = np.complex(coefs[4] * 1.e-6 + coefs[5] * j)
 	
 	for layer in xrange(coefs[0]):
 		pj[:, layer + 1] = -4. * math.pi * (coefs[4 * layer + 9] * 1.e-6 + coefs[4*layer + 10] * j - SLDfronting)
+		roughnesses[layer] = np.fabs(coefs[4 * layer + 11])
 		
+	roughnesses[-1] = coefs[7]
+	
 	pj[:, numlayers + 1] = -4 * math.pi * (SLDbacking - SLDfronting);
 
 	qq2 = np.power(qvals, 2.) / 4.
 	pj[:,:] += qq2[:, np.newaxis]
 	
 	pj = np.sqrt(pj)
-	
+
+	rj = (pj[:, :-1] - pj[:, 1:]) / (pj[:, :-1] + pj[:, 1:])
+
+	rj *= np.exp(pj[:, :-1] * pj[:, 1:] * -2 * roughnesses)
 	
 	
 	
