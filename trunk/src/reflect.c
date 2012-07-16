8692f5083c97400995e76628190aec7f2ba15155
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2012-07-16 23:20:50 +0000

    newer reflect

--- trunk/src/reflect.c
+++ trunk/src/reflect.c
@@ -1,4 +1,22 @@
 #include "refcalc.h"
+#include "Python.h"
+
 void abeles(double *yP, int oo, double *coefP, int n, double *xP, int pp) {
-	 AbelesCalc_ImagAll(coefP, n, yP, xP, pp, 0, 0, 0);
+	
+	int err = 0;
+	
+	if(oo != pp){
+		PyErr_Format(PyExc_Exception,
+				 "arg1 needs to be same size as number of values in arg3");
+		err = 2;
+	}
+	if(n>0){
+		if (4 * (int)coefP[0] + 8 != n) {
+			PyErr_Format(PyExc_Exception,
+				"arg2 needs to be length 4 * arg2[0] + 8");
+			err = 2;
+		}
+	}
+	if(!err)
+		AbelesCalc_ImagAll(coefP, n, yP, xP, pp, 0, 0, 0);
 }
