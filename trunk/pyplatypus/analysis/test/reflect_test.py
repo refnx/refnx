3d21e19f1d633ab7e11263308d3046d07f06d73c
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2012-07-24 12:00:49 +0000

    added tests

--- trunk/pyplatypus/analysis/test/reflect_test.py
+++ trunk/pyplatypus/analysis/test/reflect_test.py
@@ -1,11 +1,9 @@
 import unittest
 import pyplatypus.analysis.reflect as reflect
 import numpy as np
-import pyplatypus.analysis.energyfunctions as energyfunctions
-import pyplatypus.analysis.DEsolver as DEsolver
 import numpy.testing as npt
 
-class TestAnalysis(unittest.TestCase):
+class TestReflect(unittest.TestCase):
 
     def setUp(self):
         self.coefs = np.zeros((12))
@@ -35,35 +33,7 @@ class TestAnalysis(unittest.TestCase):
         calc = reflect.abeles(qvals.flatten(), self.coefs, **{'dqvals':dqvals.flatten()})
 
         npt.assert_almost_equal(calc, rvals.flatten())
-        
-    def test_differential_evolutionfit(self):
-        '''
-            test differential evolution fitting process
-        '''
-        theoretical = np.loadtxt('c_PLP0011859_q.txt')
-
-        qvals, rvals, evals = np.hsplit(theoretical, 3)
-        
-        coefs = np.zeros((16))
-        coefs[0] = 2
-        coefs[1] = 1.
-        coefs[2] = 2.07
-        coefs[4] = 6.36
-        coefs[7] = 3
-        coefs[8] = 300
-        coefs[9] = 3.47
-        coefs[11] = 4
-        coefs[12] = 250
-        coefs[13] = 2
-        coefs[15] = 4
-        
-        a = energyfunctions.ReflectivityFitObject(qvals, rvals, evals, reflect.abeles, coefs)
-        limits = np.zeros((2,16))
-        limits[1:, ] = 2*coefs
-        limits[:,0] = 2
-        de = DEsolver.DEsolver(limits, energyfunctions.energy_for_fitting, a)
-        print de.solve()
-        
+         
 
 if __name__ == '__main__':
     unittest.main()
