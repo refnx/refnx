eadb20f64fd93cf875a6f704c0b23d4834a94bf8
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2012-08-13 23:35:31 +0000

    creating a pyside GUI for Motofit analysis

---
+++ trunk/qt/motofit.py
@@ -0,0 +1,16 @@
+import sys
+from PySide import QtCore, QtGui
+
+from MotofitWin import Ui_MainWindow
+
+class MyMainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MyMainWindow()
+    myapp.show()
+    sys.exit(app.exec_())
