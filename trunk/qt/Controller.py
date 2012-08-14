a224c6db783d9a4fbc2922fed86d38b03c340760
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2012-08-14 10:28:11 +0000

    removing view

--- trunk/qt/Controller.py
+++ trunk/qt/Controller.py
@@ -6,8 +6,8 @@ class MyMainWindow(QtGui.QMainWindow):
         super(MyMainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.modifyGui()
         self.errorHandler = QtGui.QErrorMessage()
+        self.modifyGui()
 
     @QtCore.Slot()
     def on_do_fit_button_clicked(self):
@@ -71,12 +71,17 @@ class MyMainWindow(QtGui.QMainWindow):
         """
         row = self.ui.layerparams_tableWidget.currentRow()
         col = self.ui.layerparams_tableWidget.currentColumn()
-        if (row == 0 and col == 0) or (row == self.ui.layerparams_tableWidget.rowCount() - 1 and col == 0):
+        numrows = self.ui.layerparams_tableWidget.rowCount()
+        numcols = self.ui.layerparams_tableWidget.columnCount()
+        if row < 0 or col < 0:
+            return
+            
+        if (row == 0 and col == 0) or (row == numrows - 1 and col == 0) or (row == 0 and col == numcols - 1):
             arg_1.setText("")
             return
             
         validator = QtGui.QDoubleValidator()
-        if validator.validate(arg_1.text(), 1)[0] is QtGui.QValidator.State.Acceptable:
+        if validator.validate(arg_1.text(), 1)[0] == QtGui.QValidator.State.Acceptable:
             #update model
             pass
         else:
@@ -86,19 +91,32 @@ class MyMainWindow(QtGui.QMainWindow):
         self.ui.baseparams_tableWidget.setHorizontalHeaderLabels(['number of layers', 'scale', 'background'])
         header = self.ui.baseparams_tableWidget.horizontalHeader()
         header.setResizeMode(QtGui.QHeaderView.Stretch)
-        
+
+        initvals = ["1", "1.0", "1e-7"]
+        for cidx in xrange(3):
+            wi = QtGui.QTableWidgetItem(initvals[cidx])
+            if cidx:
+                wi.setCheckState(QtCore.Qt.Unchecked)
+            self.ui.baseparams_tableWidget.setItem(0, cidx, wi)
+
+        numrows = self.ui.layerparams_tableWidget.rowCount()
+        numcols = self.ui.layerparams_tableWidget.columnCount()
         self.ui.layerparams_tableWidget.setHorizontalHeaderLabels(['thickness', 'sld', 'iSLD', 'roughness'])
         self.ui.layerparams_tableWidget.setVerticalHeaderLabels(['fronting', '1', 'backing'])
         
+        initvals = [" ", "0", "0", " ", "25", "3.47", "0", "3", " ", "2.07", "0", "3"]
+        idx = 0
         for ridx in xrange(self.ui.layerparams_tableWidget.rowCount()):
             for cidx in xrange(self.ui.layerparams_tableWidget.columnCount()):
-                wi = QtGui.QTableWidgetItem.setCheckState(0)
+                wi = QtGui.QTableWidgetItem(initvals[idx])
+                if not ((ridx, cidx) == (0,0) or (ridx, cidx) == (0, 3) or (ridx, cidx) == (numrows - 1, 0)):
+                    wi.setCheckState(QtCore.Qt.Unchecked)
                 self.ui.layerparams_tableWidget.setItem(ridx, cidx, wi)
+                idx += 1
                 
         header = self.ui.layerparams_tableWidget.horizontalHeader()
         header.setResizeMode(QtGui.QHeaderView.Stretch)
         header = self.ui.layerparams_tableWidget.verticalHeader()
         header.setResizeMode(QtGui.QHeaderView.Stretch)
-        self.ui.dataset_comboBox.addItem("c_PLP0000708.xml")
-        self.ui.dataset_comboBox.addItem("c_PLP0000709.xml")
+        self.ui.dataset_comboBox.addItem("theoretical")
         
