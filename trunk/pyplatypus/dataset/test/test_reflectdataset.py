1e737f635bfda582a4ea9b108d0da1455ea7aa71
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2012-08-18 02:43:27 +0000

    added test for reflect dataset

--- trunk/pyplatypus/dataset/test/test_reflectdataset.py
+++ trunk/pyplatypus/dataset/test/test_reflectdataset.py
@@ -3,10 +3,10 @@ import pyplatypus.reduce.reflectdataset as reflectdataset
 import numpy as np
 import numpy.testing as npt
 
-class TestReflect(unittest.TestCase):
+class TestReflectDataset(unittest.TestCase):
 
     def setUp(self):
-		pass
+        pass
              
     def test_load_reflectivity_XML(self):
         '''
@@ -15,9 +15,11 @@ class TestReflect(unittest.TestCase):
         
         '''
         dataset = reflectdataset.ReflectDataset()
-    	with open('pyplatypus/reduce/test/c_PLP0000708.xml') as f:
-	        dataset.load_reflectivity_XML(f)
+        with open('pyplatypus/reduce/test/c_PLP0000708.xml') as f:
+            dataset.load_reflectivity_XML(f)
         
-
+        self.assertEqual(dataset.numpoints, 90)
+        self.assertEqual(90, np.size(dataset.W_q))
+        
 if __name__ == '__main__':
     unittest.main()
