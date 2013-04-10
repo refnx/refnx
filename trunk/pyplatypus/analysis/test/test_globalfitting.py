51e65b3f798b3a87d086238e89ebbc491844cb84
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2013-04-10 20:45:27 +0000

    added global fitting code and tests

--- trunk/pyplatypus/analysis/test/test_globalfitting.py
+++ trunk/pyplatypus/analysis/test/test_globalfitting.py
@@ -34,10 +34,43 @@ class TestGlobalFitting(unittest.TestCase):
         fitted_parameters = np.array([3,5,6,7,8,9,10,11,12,13,14,15])
         
         a = reflect.ReflectivityFitObject(qvals, rvals, evals, coefs, fitted_parameters = fitted_parameters)
-        linkageMatrix = np.arange(16)
+        linkageArray = np.arange(16)
         
-        gfo = gfit.GlobalFitObject((a), linkageMatrix)
+        gfo = gfit.GlobalFitObject(tuple([a]), linkageArray)
         gfo.fit()
+        
+    def test_globalfitting_modelvals(self):
+        '''
+            test differential evolution fitting process
+        '''
+        np.seterr(invalid='raise')
+        theoretical = np.loadtxt('pyplatypus/analysis/test/c_PLP0011859_q.txt')
+
+        qvals, rvals, evals, dummy = np.hsplit(theoretical, 4)
+
+        coefs = np.zeros((16))
+        coefs[0] = 2
+        coefs[1] = 1.
+        coefs[2] = 2.07
+        coefs[4] = 6.36
+        coefs[6] = 2e-6
+        coefs[7] = 3
+        coefs[8] = 300
+        coefs[9] = 3.47
+        coefs[11] = 4
+        coefs[12] = 250
+        coefs[13] = 2
+        coefs[15] = 4
+        fitted_parameters = np.array([3,5,6,7,8,9,10,11,12,13,14,15])
+        
+        a = reflect.ReflectivityFitObject(qvals, rvals, evals, coefs, fitted_parameters = fitted_parameters)
+        linkageArray = np.arange(16)
+        
+        gfo = gfit.GlobalFitObject(tuple([a]), linkageArray)
+        gfomodel = gfo.model(coefs)
+        
+        normalmodel = a.model(coefs)
+        npt.assert_almost_equal(gfomodel, normalmodel)        
 
 
 
