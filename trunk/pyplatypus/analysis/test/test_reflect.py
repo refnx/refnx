71a90dd68e57398cc0dff8d9ee347f02f9c1a076
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2013-04-10 20:14:06 +0000

    adding global fitting

--- trunk/pyplatypus/analysis/test/test_reflect.py
+++ trunk/pyplatypus/analysis/test/test_reflect.py
@@ -50,33 +50,6 @@ class TestReflect(unittest.TestCase):
         print myrho
         npt.assert_almost_equal(myrho, rho.flatten())
          
-    def test_differential_evolutionfit(self):
-        '''
-            test differential evolution fitting process
-        '''
-        np.seterr(invalid='raise')
-        theoretical = np.loadtxt('pyplatypus/analysis/test/c_PLP0011859_q.txt')
-
-        qvals, rvals, evals = np.hsplit(theoretical, 3)
-        
-        coefs = np.zeros((16))
-        coefs[0] = 2
-        coefs[1] = 1.
-        coefs[2] = 2.07
-        coefs[4] = 6.36
-        coefs[6] = 2e-6
-        coefs[7] = 3
-        coefs[8] = 300
-        coefs[9] = 3.47
-        coefs[11] = 4
-        coefs[12] = 250
-        coefs[13] = 2
-        coefs[15] = 4
-        
-        fitted_parameters = np.array([3,5,6,7,8,9,10,11,12,13,14,15])
-        
-        a = reflect.ReflectivityFitObject(qvals, rvals, evals, coefs, fitted_parameters = fitted_parameters)
-        a.fit()
 
 
 
