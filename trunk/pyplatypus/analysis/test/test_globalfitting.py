e6380d0986f1016a7fe4b22c40762bde6f18990e
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2013-04-11 03:45:58 +0000

    added test cases for global fitting

--- trunk/pyplatypus/analysis/test/test_globalfitting.py
+++ trunk/pyplatypus/analysis/test/test_globalfitting.py
@@ -76,7 +76,7 @@ class TestGlobalFitting(unittest.TestCase):
         
     def test_globfit_modelvals_degenerate_layers(self):
         '''
-            try fitting dataset with two degenerate layers
+            try fitting dataset with a deposited layer split into two degenerate layers
         '''
         np.seterr(invalid='raise')
         theoretical = np.loadtxt('pyplatypus/analysis/test/c_PLP0011859_q.txt')
@@ -101,16 +101,61 @@ class TestGlobalFitting(unittest.TestCase):
         coefs[19] = 4
 
         
-        fitted_parameters = np.array([3,5,6,7,8,9,10,11,12,13,14,15,16,17,19])
+        fitted_parameters = np.array([6,7,8,11,12,13,15,16,17,19])
         
         a = reflect.ReflectivityFitObject(qvals, rvals, evals, coefs, fitted_parameters = fitted_parameters)
         linkageArray = np.arange(20)
         linkageArray[16] = 12
-        
+        linkageArray[17] = 16
+        linkageArray[18] = 17
+        linkageArray[19] = 18
+    
         gfo = gfit.GlobalFitObject(tuple([a]), linkageArray)
         pars, dummy, chi2 = gfo.fit() 
         npt.assert_almost_equal(pars[12], pars[16])
 
+    def test_linkageArray(self):
+        '''
+            test incorrect linkageArrays
+        '''
+        np.seterr(invalid='raise')
+        theoretical = np.loadtxt('pyplatypus/analysis/test/c_PLP0011859_q.txt')
+
+        qvals, rvals, evals, dummy = np.hsplit(theoretical, 4)
+
+        coefs = np.zeros((20))
+        coefs[0] = 3
+        coefs[1] = 1.
+        coefs[2] = 2.07
+        coefs[4] = 6.36
+        coefs[6] = 2e-6
+        coefs[7] = 3
+        coefs[8] = 30
+        coefs[9] = 3.47
+        coefs[11] = 4
+        coefs[12] = 125
+        coefs[13] = 2
+        coefs[15] = 4
+        coefs[16] = 125
+        coefs[17] = 2
+        coefs[19] = 4
+        
+        fitted_parameters = np.array([6,7,8,11,12,13,15,16,17,19])
+        
+        a = reflect.ReflectivityFitObject(qvals, rvals, evals, coefs, fitted_parameters = fitted_parameters)
+        linkageArray = np.arange(20)
+        linkageArray[16] = 12
+        linkageArray[17] = 15
+        linkageArray[18] = 17
+        linkageArray[19] = 18
+
+        npt.assert_raises(Exception, gfit.GlobalFitObject, tuple([a]), linkageArray)
+        linkageArray[17] = 16
+        linkageArray[19] = -1
+        npt.assert_raises(Exception, gfit.GlobalFitObject, tuple([a]), linkageArray)
+        linkageArray[5] = -1
+        npt.assert_raises(Exception, gfit.GlobalFitObject, tuple([a]), linkageArray)
+        
 
 if __name__ == '__main__':
     unittest.main()
