d56699c96a150aa9cb541d5d3758182202c86650
andyfaff <andyfaff@06218e9e-6077-941b-ae69-44327a7b95c6>
2013-04-12 21:13:16 +0000

    adding a test for de fitting

--- pyplatypus/analysis/test/test_globalfitting.py
+++ pyplatypus/analysis/test/test_globalfitting.py
@@ -4,20 +4,11 @@ import pyplatypus.analysis.globalfitting as gfit
 import numpy as np
 import numpy.testing as npt
 
+SEED = 1
+
 class TestGlobalFitting(unittest.TestCase):
 
     def setUp(self):
-        pass
-                 
-    def test_globalfitting(self):
-        '''
-            test differential evolution fitting process
-        '''
-        np.seterr(invalid='raise')
-        theoretical = np.loadtxt('pyplatypus/analysis/test/c_PLP0011859_q.txt')
-
-        qvals, rvals, evals, dummy = np.hsplit(theoretical, 4)
-
         coefs = np.zeros((16))
         coefs[0] = 2
         coefs[1] = 1.
@@ -31,9 +22,21 @@ class TestGlobalFitting(unittest.TestCase):
         coefs[12] = 250
         coefs[13] = 2
         coefs[15] = 4
+        self.coefs = coefs
+        pass
+                 
+    def test_globalfitting(self):
+        '''
+            test differential evolution fitting process
+        '''
+        np.seterr(invalid='raise')
+        theoretical = np.loadtxt('pyplatypus/analysis/test/c_PLP0011859_q.txt')
+
+        qvals, rvals, evals, dummy = np.hsplit(theoretical, 4)
+
         fitted_parameters = np.array([3,5,6,7,8,9,10,11,12,13,14,15])
         
-        a = reflect.ReflectivityFitObject(qvals, rvals, evals, coefs, fitted_parameters = fitted_parameters)
+        a = reflect.ReflectivityFitObject(qvals, rvals, evals, self.coefs, fitted_parameters = fitted_parameters, seed = SEED)
         linkageArray = np.arange(16)
         
         gfo = gfit.GlobalFitObject(tuple([a]), linkageArray)
@@ -47,30 +50,16 @@ class TestGlobalFitting(unittest.TestCase):
         theoretical = np.loadtxt('pyplatypus/analysis/test/c_PLP0011859_q.txt')
 
         qvals, rvals, evals, dummy = np.hsplit(theoretical, 4)
-
-        coefs = np.zeros((16))
-        coefs[0] = 2
-        coefs[1] = 1.
-        coefs[2] = 2.07
-        coefs[4] = 6.36
-        coefs[6] = 2e-6
-        coefs[7] = 3
-        coefs[8] = 30
-        coefs[9] = 3.47
-        coefs[11] = 4
-        coefs[12] = 250
-        coefs[13] = 2
-        coefs[15] = 4
         
         fitted_parameters = np.array([3,5,6,7,8,9,10,11,12,13,14,15])
         
-        a = reflect.ReflectivityFitObject(qvals, rvals, evals, coefs, fitted_parameters = fitted_parameters)
+        a = reflect.ReflectivityFitObject(qvals, rvals, evals, self.coefs, fitted_parameters = fitted_parameters)
         linkageArray = np.arange(16)
         
         gfo = gfit.GlobalFitObject(tuple([a]), linkageArray)
-        gfomodel = gfo.model(coefs)
+        gfomodel = gfo.model(self.coefs)
         
-        normalmodel = a.model(coefs)
+        normalmodel = a.model(self.coefs)
         npt.assert_almost_equal(gfomodel, normalmodel)        
 
         
