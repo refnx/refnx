13717db441e852d4eec2042ffc8eb2e093533eb3
Andrew Nelson <andyfaff@gmail.com>
2014-12-26 21:27:27 +1100

    MAINT: GlobalFitter now extends CurveFitter.

--- pyplatypus/analysis/curvefitter.py
+++ pyplatypus/analysis/curvefitter.py
@@ -81,201 +81,6 @@ def params(p0, varies=None, bounds=None, names=None, expr=None):
     return p
 
 
-class GlobalFitter(Minimizer):
-    """
-    A class for simultaneous curvefitting of multiple datasets
-    """
-    def __init__(self, datasets, constraints=(), kws=None,
-                 callback=None):
-        """
-        datasets : sequence of CurveFitter instances
-            Contains all the datasets and fitfunctions for the global fit.
-        constraints : str sequence, optional
-            Of the type 'dNpM:constraint'. Sets a constraint expression for
-            parameter M in dataset N.  The constraint 'd2p3:d0p1' constrains
-            parameter 3 in dataset 2 to be equal to parameter 1 in dataset 0.
-        kws : dict, optional
-            Extra minimization keywords to be passed to the minimizer of
-            choice.
-        callback : callable, optional
-            Function called at each step of the minimization. Has the signature
-            ``callback(params, iter, resid)``
-
-        """
-        min_kwds = {}
-        if kws is not None:
-            min_kwds = kws
-
-        for dataset in datasets:
-            if not isinstance(dataset, CurveFitter):
-                raise ValueError('All items in curve_fitter_list must be '
-                                 'instances of CurveFitter')
-
-        self.datasets = datasets
-
-        self.new_param_names = []
-        p = Parameters()
-        for i, dataset in enumerate(self.datasets):
-            # add all the parameters for a given dataset
-            new_names = {}
-            for j, item in enumerate(dataset.params.items()):
-                old_name = item[0]
-                param = item[1]
-                new_name = old_name + '_d%dp%d' % (i, j)
-                new_names[new_name] = old_name
-
-                p.add(new_name,
-                      value=param.value,
-                      vary=param.vary,
-                      min=param.min,
-                      max=param.max,
-                      expr=param.expr)
-
-            self.new_param_names.append(new_names)
-
-            # if there are any expressions they have to be updated
-            # iterate through all the parameters in the dataset
-            old_names = dict((v, k) for k, v in new_names.items())
-            for i, param in enumerate(dataset.params.values()):
-                expr = param.expr
-                new_name = old_names[param.name]
-                # if it's got an expression you'll have to update it
-                if expr is not None:
-                    # see if any of the old names are in there.
-                    for old_name in old_names:
-                        regex = re.compile('(%s)' % old_name)
-                        if regex.search(expr):
-                            new_expr_name = old_names[old_name]
-                            new_expr = expr.replace(old_name, new_expr_name)
-                            p[new_name].set(expr=new_expr)
-
-        # now set constraints/linkages up. They're specified as dNpM:constraint
-        regex = re.compile('d([0-9]*)p([0-9]*)[\s]*:[\s]*(.*)')
-        data_regex = re.compile('d([0-9]*)p([0-9]*)')
-
-        all_names = p.valuesdict().keys()
-        for constraint in constraints:
-            r = regex.search(constraint)
-            if r:
-                N, M = int(r.groups()[0]), int(r.groups()[1])
-                const = r.groups()[2]
-
-                n_left = re.compile('.*(_d%dp%d)' % (N, M))
-                # search all the names
-                name_left = [m.group(0) for l in all_names for m in
-                              [n_left.search(l)] if m]
-                if not len(name_left):
-                    continue
-
-                # now search for datasets mentioned in constraint
-                d_mentioned = data_regex.findall(const)
-                for d in d_mentioned:
-                    # we need to replace all datasets mentioned in constraint
-                    n_right = re.compile('.*(_d%sp%s)' % (d[0], d[1]))
-                    name_right = [m.group(0) for l in all_names for m in
-                                  [n_right.search(l)] if m]
-
-                    if len(name_right):
-                        const = const.replace(
-                                    'd%sp%s' % (d[0], d[1]),
-                                    name_right[0])
-
-                p[name_left[0]].set(expr=const)
-
-        self.params = p
-        super(GlobalFitter, self).__init__(self.residuals,
-                                           self.params,
-                                           iter_cb=callback,
-                                           **min_kwds)
-
-    def model(self, params):
-        """
-        Calculates the model.
-
-        Parameters
-        ----------
-        params : lmfit.Parameters instance
-            Specifies the entire parameter set, across all the datasets
-
-        Returns
-        -------
-        model : np.ndarray
-            The model.
-        """
-        values = params.valuesdict()
-        model = np.zeros(0, dtype='float64')
-        for i, dataset in enumerate(self.datasets):
-            new_names = self.new_param_names[i]
-            for new_name, old_name in new_names.items():
-                dataset.params[old_name].set(value=values[new_name])
-            model = np.append(model,
-                              dataset.model(dataset.params))
-
-        return model
-
-    def residuals(self, params):
-        """
-        Calculate the difference between the data and the model.
-        Also known as the objective function.  This function is minimized
-        during a fit.
-        residuals = (fitfunc - ydata) / edata
-
-        Parameters
-        ----------
-        params : lmfit.Parameters instance
-            Specifies the entire parameter set
-
-        Returns
-        -------
-        residuals : np.ndarray
-            The difference between the data and the model.
-        """
-        values = params.valuesdict()
-        total_residuals = np.zeros(0, dtype='float64')
-        for i, dataset in enumerate(self.datasets):
-            new_names = self.new_param_names[i]
-            for new_name, old_name in new_names.items():
-                dataset.params[old_name].set(value=values[new_name])
-            resid = (dataset.ydata - dataset.model(dataset.params))
-            resid /= dataset.edata
-            
-            total_residuals = np.append(total_residuals,
-                                        resid)
-
-        return total_residuals
-
-    def fit(self, method='leastsq'):
-        """
-        Fits the dataset.
-
-        Parameters
-        ----------
-        method : str, optional
-            Name of the fitting method to use.
-            One of:
-            'leastsq'                -    Levenberg-Marquardt (default)
-            'nelder'                 -    Nelder-Mead
-            'lbfgsb'                 -    L-BFGS-B
-            'powell'                 -    Powell
-            'cg'                     -    Conjugate-Gradient
-            'newton'                 -    Newton-CG
-            'cobyla'                 -    Cobyla
-            'tnc'                    -    Truncate Newton
-            'trust-ncg'              -    Trust Newton-CGn
-            'dogleg'                 -    Dogleg
-            'slsqp'                  -    Sequential Linear Squares Programming
-            'differential_evolution' -    differential evolution
-
-        Returns
-        -------
-        success : bool
-            Whether the fit succeeded.
-        """
-        success = self.minimize(method=method)
-        self.residuals(self.params)
-        return success
-
-
 class CurveFitter(Minimizer):
     """
     A curvefitting class that extends lmfit.Minimize
@@ -482,6 +287,176 @@ class CurveFitter(Minimizer):
         self.redchi = self.chisqr / (self.ndata - self.nvarys)
         del(self.__fun_evals)
         return MDL
+
+
+class GlobalFitter(CurveFitter):
+    """
+    A class for simultaneous curvefitting of multiple datasets
+    """
+    def __init__(self, datasets, constraints=(), kws=None,
+                 callback=None):
+        """
+        datasets : sequence of CurveFitter instances
+            Contains all the datasets and fitfunctions for the global fit.
+        constraints : str sequence, optional
+            Of the type 'dNpM:constraint'. Sets a constraint expression for
+            parameter M in dataset N.  The constraint 'd2p3:d0p1' constrains
+            parameter 3 in dataset 2 to be equal to parameter 1 in dataset 0.
+        kws : dict, optional
+            Extra minimization keywords to be passed to the minimizer of
+            choice.
+        callback : callable, optional
+            Function called at each step of the minimization. Has the signature
+            ``callback(params, iter, resid)``
+
+        """
+        min_kwds = {}
+        if kws is not None:
+            min_kwds = kws
+
+        for dataset in datasets:
+            if not isinstance(dataset, CurveFitter):
+                raise ValueError('All items in curve_fitter_list must be '
+                                 'instances of CurveFitter')
+
+        self.datasets = datasets
+
+        self.new_param_names = []
+        p = Parameters()
+        for i, dataset in enumerate(self.datasets):
+            # add all the parameters for a given dataset
+            new_names = {}
+            for j, item in enumerate(dataset.params.items()):
+                old_name = item[0]
+                param = item[1]
+                new_name = old_name + '_d%dp%d' % (i, j)
+                new_names[new_name] = old_name
+
+                p.add(new_name,
+                      value=param.value,
+                      vary=param.vary,
+                      min=param.min,
+                      max=param.max,
+                      expr=param.expr)
+
+            self.new_param_names.append(new_names)
+
+            # if there are any expressions they have to be updated
+            # iterate through all the parameters in the dataset
+            old_names = dict((v, k) for k, v in new_names.items())
+            for i, param in enumerate(dataset.params.values()):
+                expr = param.expr
+                new_name = old_names[param.name]
+                # if it's got an expression you'll have to update it
+                if expr is not None:
+                    # see if any of the old names are in there.
+                    for old_name in old_names:
+                        regex = re.compile('(%s)' % old_name)
+                        if regex.search(expr):
+                            new_expr_name = old_names[old_name]
+                            new_expr = expr.replace(old_name, new_expr_name)
+                            p[new_name].set(expr=new_expr)
+
+        # now set constraints/linkages up. They're specified as dNpM:constraint
+        regex = re.compile('d([0-9]*)p([0-9]*)[\s]*:[\s]*(.*)')
+        data_regex = re.compile('d([0-9]*)p([0-9]*)')
+
+        all_names = p.valuesdict().keys()
+        for constraint in constraints:
+            r = regex.search(constraint)
+            if r:
+                N, M = int(r.groups()[0]), int(r.groups()[1])
+                const = r.groups()[2]
+
+                n_left = re.compile('.*(_d%dp%d)' % (N, M))
+                # search all the names
+                name_left = [m.group(0) for l in all_names for m in
+                              [n_left.search(l)] if m]
+                if not len(name_left):
+                    continue
+
+                # now search for datasets mentioned in constraint
+                d_mentioned = data_regex.findall(const)
+                for d in d_mentioned:
+                    # we need to replace all datasets mentioned in constraint
+                    n_right = re.compile('.*(_d%sp%s)' % (d[0], d[1]))
+                    name_right = [m.group(0) for l in all_names for m in
+                                  [n_right.search(l)] if m]
+
+                    if len(name_right):
+                        const = const.replace(
+                                    'd%sp%s' % (d[0], d[1]),
+                                    name_right[0])
+
+                p[name_left[0]].set(expr=const)
+
+        self.params = p
+        xdata = [dataset.xdata for dataset in datasets]
+        ydata = [dataset.ydata for dataset in datasets]
+        edata = [dataset.edata for dataset in datasets]
+        super(GlobalFitter, self).__init__(self.params,
+                                           xdata,
+                                           ydata,
+                                           None,
+                                           edata=edata,
+                                           callback=callback,
+                                           kws=min_kwds)
+
+    def model(self, params):
+        """
+        Calculates the model.
+
+        Parameters
+        ----------
+        params : lmfit.Parameters instance
+            Specifies the entire parameter set, across all the datasets
+
+        Returns
+        -------
+        model : np.ndarray
+            The model.
+        """
+        values = params.valuesdict()
+        model = np.zeros(0, dtype='float64')
+        for i, dataset in enumerate(self.datasets):
+            new_names = self.new_param_names[i]
+            for new_name, old_name in new_names.items():
+                dataset.params[old_name].set(value=values[new_name])
+            model = np.append(model,
+                              dataset.model(dataset.params))
+
+        return model
+
+    def residuals(self, params):
+        """
+        Calculate the difference between the data and the model.
+        Also known as the objective function.  This function is minimized
+        during a fit.
+        residuals = (fitfunc - ydata) / edata
+
+        Parameters
+        ----------
+        params : lmfit.Parameters instance
+            Specifies the entire parameter set
+
+        Returns
+        -------
+        residuals : np.ndarray
+            The difference between the data and the model.
+        """
+        values = params.valuesdict()
+        total_residuals = np.zeros(0, dtype='float64')
+        for i, dataset in enumerate(self.datasets):
+            new_names = self.new_param_names[i]
+            for new_name, old_name in new_names.items():
+                dataset.params[old_name].set(value=values[new_name])
+            resid = (dataset.ydata - dataset.model(dataset.params))
+            resid /= dataset.edata
+            
+            total_residuals = np.append(total_residuals,
+                                        resid)
+
+        return total_residuals
 
 
 if __name__ == '__main__':
