aa7b09a43cb3d5f61819dc72a98c6281a53734e6
Andrew Nelson <andyfaff@gmail.com>
2015-11-12 15:18:51 +1100

    stuff

--- refnx/reduce/reduce.py
+++ refnx/reduce/reduce.py
@@ -481,7 +481,7 @@ def reduce_stitch(reflect_list, direct_list, norm_file_num=None,
         with open(fname, 'wb') as f:
             combined_dataset.save(f)
         fname = 'c_PLP{0:07d}.xml'.format(reflect_list[0])
-        with open(fname, 'w') as f:
+        with open(fname, 'wb') as f:
             combined_dataset.save_xml(f)
 
     return combined_dataset, fname
