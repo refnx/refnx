5349d456f8fc21ad453bad09138079d70e6a2ad0
Andrew Nelson <andyfaff@gmail.com>
2016-10-07 14:43:25 +1100

    MAINT: modified cython reflectivity code

--- src/_creflect.pyx
+++ src/_creflect.pyx
@@ -60,68 +60,72 @@ def abeles(np.ndarray x,
     return y
 
 """
-This is around 7 times slower than wrapped C.
-
+# Slower than the python version!
 @cython.boundscheck(False)
 @cython.cdivision(False)
 def reflect(np.ndarray[DTYPE_t, ndim=1] x,
-            np.ndarray[DTYPE_t, ndim=2] w,
-            double scale=1.0, bkg=0.):
-
+             np.ndarray[DTYPE_t, ndim=2] w,
+             double scale=1.0, double bkg=0.):
     if w.shape[1] != 4 or w.shape[0] < 2:
         raise ValueError("Parameters for reflection must have shape (>2, 4)")
     assert w.dtype == np.float64 and x.dtype == np.float64
-
     cdef int nlayers = w.shape[0] - 2
     cdef int npoints = x.shape[0]
     cdef int layer
-
     cdef np.ndarray[np.complex128_t, ndim=1] y = np.zeros(npoints,
                              np.complex128)
-
     cdef np.ndarray[np.complex128_t, ndim=2] kn = np.zeros(
                             (npoints, w.shape[0]), np.complex128)
-
-    cdef np.ndarray[np.complex128_t, ndim=2] rj = np.zeros(
-                            (npoints, w.shape[0] - 1), np.complex128)
-
-    cdef np.ndarray[np.complex128_t, ndim=2] beta = np.ones(
-                            (npoints, w.shape[0] - 1), np.complex128)
-
+    cdef np.ndarray[np.complex128_t, ndim=1] rj = np.zeros(
+                            (npoints), np.complex128)
     cdef np.ndarray[np.complex128_t, ndim=1] SLD = np.zeros(
                             (w.shape[0]), np.complex128)
-
-    cdef np.ndarray[np.complex128_t, ndim=3] MRtotal = np.zeros(
-                            (npoints, 2, 2), np.complex128)
-
-    cdef np.ndarray[np.complex128_t, ndim=4] MI = np.zeros(
-                            (npoints, nlayers + 1, 2, 2), np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mi00 = np.ones_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mi01 = np.zeros_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mi10 = np.zeros_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mi11 = np.ones_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mrtot00 = np.ones_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mrtot01 = np.zeros_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mrtot10 = np.zeros_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] mrtot11 = np.ones_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] p0 = np.zeros_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] p1 = np.zeros_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] k = np.zeros_like(y, np.complex128)
+    cdef np.ndarray[np.complex128_t, ndim=1] k_next = np.zeros_like(y, np.complex128)
 
     SLD[:] += 4 * np.pi * (w[:, 1] - w[0, 1] + 1j * (w[:, 2] - w[0, 2])) * 1.e-6
-
     kn[:] = np.sqrt(x.flatten()[:, np.newaxis]**2 / 4. - SLD)
 
-    rj += (kn[:, :-1] - kn[:, 1:]) / (kn[:, :-1] + kn[:, 1:])
-    rj *= np.exp(kn[:, :-1] * kn[:, 1:] * -2. * w[1:, 3] * w[1:, 3])
+    k = kn[:, 0]
+    for idx in range(1, nlayers + 2):
+        k_next = kn[:, idx]
+        rj = (k - k_next) / (k + k_next)
+        rj *= np.exp(k * k_next * -2. * w[idx, 3] ** 2)
 
-    if nlayers:
-        #the last term in this statement is the thickness.
-        beta[:, 1:] = np.exp(kn[:, 1::-2]
-                             * 1j * np.fabs(w[1::-2, 0])[np.newaxis,])
+        # work out characteristic matrix of layer
+        if idx - 1:
+            mi00 = np.exp(k * 1j * np.fabs(w[idx - 1, 0]))
+            mi11 = np.exp(k * -1j * np.fabs(w[idx - 1, 0]))
 
-    MI[:, :, 0, 0] = beta
-    MI[:, :, 1, 1] = 1. / beta
-    MI[:, :, 0, 1] = rj * beta
-    MI[:, :, 1, 0] = rj * MI[:, :, 1, 1]
+        mi10 = rj * mi00
+        mi01 = rj * mi11
 
-    #start doing the matrix multiplication.  Initialise with the first interface
-    MRtotal[:] = MI[:, 0]
+        # matrix multiply mrtot and mi
+        p0 = mrtot00 * mi00 + mrtot10 * mi01
+        p1 = mrtot00 * mi10 + mrtot10 * mi11
+        mrtot00 = p0
+        mrtot10 = p1
 
-    for layer in range(1, nlayers + 1):
-        MRtotal = np.einsum('...ij,...jk->...ik', MRtotal, MI[:, layer])
+        p0 = mrtot01 * mi00 + mrtot11 * mi01
+        p1 = mrtot01 * mi10 + mrtot11 * mi11
 
-    y = ((MRtotal[:, 1, 0] * np.conj(MRtotal[:, 1, 0]))
-         / (MRtotal[:, 0, 0] * np.conj(MRtotal[:, 0, 0])))
+        mrtot01 = p0
+        mrtot11 = p1
 
-    return scale * y.real + bkg
+        k = k_next
+
+    y = (mrtot01 * np.conj(mrtot01)) / (mrtot00 * np.conj(mrtot00)))
+    y *= scale
+    y += bkg
+    return y.real
 """
