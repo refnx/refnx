55b83d9aaf39292b591e8454fcee1e44e91e2954
Andrew Nelson <andyfaff@gmail.com>
2017-05-04 19:10:55 +1000

    MAINT: adding plotting in slim

--- slim/plot.py
+++ slim/plot.py
@@ -6,6 +6,9 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
+import numpy as np
+from refnx.dataset import ReflectDataset
+
 
 class SlimPlotWindow(QDialog):
     def __init__(self, parent=None):
@@ -37,12 +40,17 @@ class SlimPlotWindow(QDialog):
         layout.addWidget(self.refresh_button)
         self.setLayout(layout)
 
-        self.files_displayed = []
+        self.files_displayed = {}
         self.data_directory = '~/'
 
     def refresh(self):
-        if self.files_displayed:
-            self.plot(files_to_display=self.files_displayed)
+        # refresh a dataset (may have been re-reduced)
+        for k, v in self.files_displayed.items():
+            dataset = v[0]
+            line = v[1]
+            dataset.refresh()
+            self.adjustErrbarxy(line, *dataset.data[0:3])
+        self.canvas.draw()
 
     @pyqtSlot(str)
     def data_directory_changed(self, directory):
@@ -50,18 +58,16 @@ class SlimPlotWindow(QDialog):
 
     def plot(self, files_to_display=False):
         if not files_to_display:
-            files_to_display = QtWidgets.QFileDialog.getOpenFileNames(
+            files = QtWidgets.QFileDialog.getOpenFileNames(
                 self,
                 "Select reflectometry data files to plot",
                 directory=self.data_directory,
                 filter='Reflectometry files (*.xml)')
+            files_to_display = files[0]
 
-        if not files_to_display[0]:
+        if not files_to_display:
             # could've cancelled the file dialogue
             return
-
-        # load each file and display it
-        TODO
 
         # instead of ax.hold(False)
         self.figure.clear()
@@ -69,8 +75,36 @@ class SlimPlotWindow(QDialog):
         # create an axis
         ax = self.figure.add_subplot(111)
 
-        # plot data
-        ax.plot(data, '*-')
+        displayed = {}
+
+        # load each file and display it
+        for file in files_to_display:
+            dataset = ReflectDataset(file)
+
+            # plot data
+            line = ax.errorbar(*dataset.data[0:3], label=dataset.name)
+
+            displayed[dataset.name] = (dataset, line)
+
+        # add legend and plot log-lin
+        ax.legend()
+        ax.set_yscale('log')
+
+        self.files_displayed = displayed
 
         # refresh canvas
         self.canvas.draw()
+
+    def adjustErrbarxy(self, errobj, x, y, y_error):
+        """for adjusting error bar plot with updated data"""
+
+        # caplines and barsy have len > 1 if xerrors are displayed
+        ln, caplines, barsy = errobj
+        x_base = x
+        y_base = y
+
+        yerr_top = y_base + y_error
+        yerr_bot = y_base - y_error
+
+        new_segments_y = [np.array([[x, yt], [x,yb]]) for x, yt, yb in zip(x_base, yerr_top, yerr_bot)]
+        barsy[0].set_segments(new_segments_y)
