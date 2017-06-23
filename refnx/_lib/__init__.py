b7649a4874676d3403c5d376f5f440e109218852
Andrew Nelson <andyfaff@gmail.com>
2017-06-23 10:49:09 +1000

    ENH: adding a file context manager

--- refnx/_lib/__init__.py
+++ refnx/_lib/__init__.py
@@ -1,2 +1,3 @@
-from refnx._lib.util import TemporaryDirectory, preserve_cwd
+from refnx._lib.util import TemporaryDirectory, preserve_cwd, flatten, unique
+
 __all__ = [s for s in dir() if not s.startswith('_')]
