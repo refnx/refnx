042a36852859e4b6fcfa072a929a906b9bd90739
Andrew Nelson <andyfaff@gmail.com>
2017-11-16 12:06:28 +1100

    ENH: add cython unique, flatten

--- refnx/_lib/__init__.py
+++ refnx/_lib/__init__.py
@@ -2,11 +2,18 @@ from __future__ import division, absolute_import
 
 import numpy.testing
 
-from refnx._lib.util import (TemporaryDirectory, preserve_cwd, flatten, unique,
-                             possibly_open_file, possibly_create_pool)
+from refnx._lib.util import (TemporaryDirectory, preserve_cwd, possibly_open_file,
+                             possibly_create_pool)
 from refnx._lib._numdiff import approx_hess2
 
 from refnx._lib._testutils import PytestTester
+
+try:
+    from refnx._lib._cutil import c_unique as unique
+    from refnx._lib._cutil import c_flatten as flatten
+except ImportError:
+    from refnx._lib.util import unique, flatten
+
 
 test = PytestTester(__name__)
 del PytestTester
