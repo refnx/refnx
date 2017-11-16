d891240aaaab51ee38db425c999f8cf412e59a9c
Andrew Nelson <andyfaff@gmail.com>
2017-11-16 12:07:30 +1100

    PEP8

--- refnx/_lib/__init__.py
+++ refnx/_lib/__init__.py
@@ -2,8 +2,8 @@ from __future__ import division, absolute_import
 
 import numpy.testing
 
-from refnx._lib.util import (TemporaryDirectory, preserve_cwd, possibly_open_file,
-                             possibly_create_pool)
+from refnx._lib.util import (TemporaryDirectory, preserve_cwd,
+                             possibly_open_file, possibly_create_pool)
 from refnx._lib._numdiff import approx_hess2
 
 from refnx._lib._testutils import PytestTester
