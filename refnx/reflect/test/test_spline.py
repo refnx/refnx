c86f3a939c240a533601a1fd6498c5fb4f6e3356
Andrew Nelson <andyfaff@gmail.com>
2017-11-17 15:37:15 +1100

    MAINT: fixing issues in Spline

--- refnx/reflect/test/test_spline.py
+++ refnx/reflect/test/test_spline.py
@@ -1,6 +1,9 @@
 import numpy as np
-from numpy.testing import assert_allclose, assert_equal, assert_almost_equal
+from numpy.testing import (assert_allclose, assert_equal, assert_almost_equal,
+                           assert_)
 from refnx.reflect import SLD, Slab, Structure, Spline
+from refnx.analysis import Parameter
+from refnx._lib import flatten
 
 
 class TestReflect(object):
@@ -14,10 +17,9 @@ class TestReflect(object):
         # smoke test to make Spline at least gives us something
         a = Spline(100, [2],
                    [0.5], self.left, self.right, self.solvent, zgrad=False,
-                   microslab_max_thickness=2)
+                   microslab_max_thickness=1)
         b = a.slabs
         assert_equal(b[:, 2], 0)
-        assert_equal(b[:, 3], 0.5)
 
         # microslabs are assessed in the middle of the slab
         assert_equal(b[0, 1], a(0.5 * b[0, 0]))
@@ -25,12 +27,24 @@ class TestReflect(object):
         # with the ends turned off the profile should be a straight line
         assert_equal(a(50), 2.0)
 
+        # construct a structure
+        a = Spline(100, [2., 3., 4.],
+                   [0.25] * 3, self.left, self.right, self.solvent,
+                   zgrad=False, microslab_max_thickness=1)
+
+        s = self.left | a | self.right | self.solvent
+        # calculate an SLD profile
+        s.sld_profile()
+        # ask for the parameters
+        for p in flatten(s.parameters):
+            assert_(isinstance(p, Parameter))
+
     def test_left_right_influence(self):
         # make sure that if the left and right components change, so does the
         # spline
         a = Spline(100, [2],
                    [0.5], self.left, self.right, self.solvent, zgrad=False,
-                   microslab_max_thickness=2)
+                   microslab_max_thickness=1)
 
         # change the SLD of the left component, spline should respond
         self.left.sld.real.value = 2.
