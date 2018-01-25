9a88eb0a925745c60ef04ea64ce68e15a6e12537
Andrew Nelson <andyfaff@gmail.com>
2018-01-25 15:08:32 +1100

    MAINT: setup.py

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ info = {
         'packages': packages,
         'include_package_data': True,
         'setup_requires': ['numpy'],
-        'install_requires': ['numpy', 'scipy', 'emcee>=3', 'six',
+        'install_requires': ['numpy', 'scipy', 'emcee', 'six',
                              'uncertainties', 'pandas']
         }
 
