BUG: fix covariance calculation objective.covar leading diagonal was correct, off diagonal elements were scaled incorrectly. A test is added to check covariance against optimize.least_squares

refnx/analysis/objective.py
@@ -573,16 +573,6 @@ class Objective(BaseObjective):
         """
         _pvals = np.array(self.varying_parameters())
 
-        # hess = nd.Hessian(self.chisqr)(_pvals)
-
-        # if the initial attempt with unscaled values fails, than fallover
-        # to situation where we scale the parameters to 1. The initial attempt
-        # could fail, for example, if the data is scaled with a log10
-        # transform. If the nd.MaxStepGenerator makes some points negative then
-        # the transform will complain because it's trying to take the log of a
-        # negative number. By scaling the pvals to 1 in the Jacobian
-        # calculation we can get around this because the stepper won't make any
-        # of the parameter values change sign (step too small).
         used_residuals_scaler = False
 
         def residuals_scaler(vals):
@@ -594,9 +584,10 @@ class Objective(BaseObjective):
             used_residuals_scaler = True
         except FloatingPointError:
             jac = approx_derivative(self.residuals, _pvals)
-
-        # if len(_pvals) == 1:
-        #     jac = jac.T
+        finally:
+            # using approx_derivative changes the state of the objective
+            # parameters have to make sure they're set at the end
+            self.setp(_pvals)
 
         # need to create this because GlobalObjective does not have
         # access to all the datapoints being fitted.
@@ -608,11 +599,9 @@ class Objective(BaseObjective):
 
         if used_residuals_scaler:
             # unwind the scaling.
-            covar *= _pvals ** 2
+            covar = covar * np.atleast_2d(_pvals) * np.atleast_2d(_pvals).T
 
-        self.setp(_pvals)
         scale = 1.
-
         # scale by reduced chi2 if experimental uncertainties weren't used.
         if not (self.use_weights and self.weighted):
             scale = (self.chisqr() /

refnx/analysis/test/test_objective.py
@@ -6,13 +6,14 @@ import pickle
 from multiprocessing.reduction import ForkingPickler
 import os
 
-from scipy.optimize import minimize
 import numpy as np
+from scipy.optimize import minimize, least_squares
+from scipy.optimize._numdiff import approx_derivative
 from numpy.testing import (assert_almost_equal, assert_equal, assert_,
                            assert_allclose)
 
 from refnx.analysis import (Parameter, Model, Objective, BaseObjective,
-                            Transform)
+                            Transform, Parameters)
 from refnx.dataset import Data1D, ReflectDataset
 from refnx.util import ErrorProp as EP
 from refnx._lib import emcee
@@ -21,6 +22,11 @@ from refnx._lib import emcee
 def line(x, params, *args, **kwds):
     p_arr = np.array(params)
     return p_arr[0] + x * p_arr[1]
+
+
+def gauss(x, p0):
+    p = np.array(p0)
+    return p[0] + p[1] * np.exp(-((x - p[2]) / p[3])**2)
 
 
 def lnprob_extra(model, data):
@@ -313,3 +319,60 @@ class TestObjective(object):
         # uncertainties3 = np.sqrt(np.diag(covar3))
         # assert_almost_equal(uncertainties3, uncertainties)
         # assert(False)
+
+    def test_covar(self):
+        # checks objective.covar against optimize.least_squares covariance.
+        path = os.path.dirname(os.path.abspath(__file__))
+
+        theoretical = np.loadtxt(os.path.join(path, 'gauss_data.txt'))
+        xvals, yvals, evals = np.hsplit(theoretical, 3)
+        xvals = xvals.flatten()
+        yvals = yvals.flatten()
+        evals = evals.flatten()
+
+        p0 = np.array([0.1, 20., 0.1, 0.1])
+        names = ['bkg', 'A', 'x0', 'width']
+        bounds = [(-1, 1), (0, 30), (-5., 5.), (0.001, 2)]
+
+        params = Parameters(name="gauss_params")
+        for p, name, bound in zip(p0, names, bounds):
+            param = Parameter(p, name=name)
+            param.range(*bound)
+            param.vary = True
+            params.append(param)
+
+        model = Model(params, fitfunc=gauss)
+        data = Data1D((xvals, yvals, evals))
+        objective = Objective(model, data)
+
+        # first calculate least_squares jac/hess/covariance matrices
+        res = least_squares(objective.residuals, np.array(params),
+                            jac='3-point')
+
+        hess_least_squares = np.matmul(res.jac.T, res.jac)
+        covar_least_squares = np.linalg.inv(hess_least_squares)
+
+        # now calculate corresponding matrices by hand, to see if the approach
+        # concurs with least_squares
+        objective.setp(res.x)
+        _pvals = np.array(res.x)
+
+        def residuals_scaler(vals):
+            return np.squeeze(objective.residuals(_pvals * vals))
+
+        with np.errstate(invalid='raise'):
+            jac = approx_derivative(residuals_scaler, np.ones_like(_pvals))
+
+        hess = np.matmul(jac.T, jac)
+        covar = np.linalg.inv(hess)
+
+        covar = covar * np.atleast_2d(_pvals) * np.atleast_2d(_pvals).T
+
+        assert_allclose(covar, covar_least_squares)
+
+        # check that objective.covar corresponds to the least_squares
+        # covariance matrix
+        objective.setp(res.x)
+        _pvals = np.array(res.x)
+        covar_objective = objective.covar()
+        assert_allclose(covar_objective, covar_least_squares)