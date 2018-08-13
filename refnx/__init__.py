a568f174d8fb0034fa11c0d4fad0ae9e682b6486
Andrew Nelson <andyfaff@gmail.com>
2018-08-13 15:43:20 +1000

    MAINT: fix refnx main import

--- refnx/__init__.py
+++ refnx/__init__.py
@@ -2,14 +2,14 @@
 refnx: Neutron and X-ray reflectometry analysis in Python
 =========================================================
 Documentation is available in the docstrings and
-online at http://refnx.github.io/
+online at https://readthedocs.org/projects/refnx/
 """
 
 from __future__ import division, print_function, absolute_import
 from refnx.version import version as __version__
 from numpy.testing import Tester
 
-__all__ = ['test']
+#__all__ = ['test']
 
 from refnx._lib._testutils import PytestTester
 
