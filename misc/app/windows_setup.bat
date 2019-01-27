a1521442a902cfbd812ad98224d7b1aa78b4c0dc
Andrew Nelson <andyfaff@gmail.com>
2019-01-27 18:32:02 +1100

    BLD: remove historical file cruft [ci skip]

--- misc/app/windows_setup.bat
+++
@@ -1,1 +0,0 @@
-c:\Python27\Scripts\cxfreeze qt\motofit.py --target-dir dist --base-name Win32GUI --include-modules atexit,PySide.QtNetwork,scipy.special._ufuncs,scipy.integrate,scipy.integrate.vode,scipy.special._ufuncs_cxx,scipy.integrate.lsoda,scipy.sparse.csgraph._validation,scipy.sparse.linalg.dsolve.umfpack
