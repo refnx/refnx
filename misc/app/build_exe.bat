cc2bff43223febd756e228a6b6ac0ef7041beeb0
Andrew Nelson <andyfaff@gmail.com>
2019-02-19 19:21:35 +1100

    BLD: fix build script nit [skip ci]

--- misc/app/build_exe.bat
+++ misc/app/build_exe.bat
@@ -4,6 +4,6 @@ call conda create -y -n app python=3.7 pip
 call activate app
 pip install pyqt5 numpy==1.15.2 scipy cython matplotlib periodictable pyinstaller
 pip install ../../.
-if exist dist( rmdir /s/q dist)
-if exist dist( rmdir /s/q build)
+if exist dist ( rmdir /s/q dist)
+if exist build ( rmdir /s/q build)
 pyinstaller motofit.spec
