2f3e3d27a7a09c75ed2681e82524800d73804bc8
Andrew Nelson <andyfaff@gmail.com>
2019-02-20 22:03:39 +1100

    BLD: use conda-forge numpy

--- misc/app/build_exe.bat
+++ misc/app/build_exe.bat
@@ -1,9 +1,9 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 call conda env remove -y -n app
-call conda create -y -n app python=3.7
+call conda create -y -n app python=3.7 pip
 call activate app
-call conda install -y pip
-pip install pyqt5 numpy==1.15.2 scipy cython matplotlib periodictable pyinstaller
+call conda install -y -c conda-forge numpy
+pip install pyqt5 cython matplotlib periodictable pyinstaller scipy
 pip install ../../.
 if exist dist ( rmdir /s/q dist)
 if exist build ( rmdir /s/q build)
