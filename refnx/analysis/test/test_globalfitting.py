BUG: reflectivity calculation was using wrong branch cut

refnx/analysis/test/test_globalfitting.py
@@ -61,16 +61,17 @@ class TestGlobalFitting(object):
 
         self.objective.transform = Transform('logY')
 
+        starting = np.array(self.objective.parameters)
         with np.errstate(invalid='raise'):
             g = CurveFitter(self.global_objective)
-            res_g = g.fit(method='differential_evolution', seed=1, maxiter=10)
+            res_g = g.fit()
 
+            # need the same starting point
+            self.objective.setp(starting)
             f = CurveFitter(self.objective)
-            res_f = f.fit(method='differential_evolution', seed=1, maxiter=10)
+            res_f = f.fit()
 
-            # individual and global should give the same fit. Because we use DE
-            # there is no dependence on starting point, so long as we set a
-            # seed.
+            # individual and global should give the same fit.
             assert_almost_equal(res_g.x, res_f.x)
 
     def test_multipledataset_corefinement(self):

refnx/reflect/_reflect.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
+TINY = np.finfo(np.float64).tiny
+
+
 """
 import numpy as np
 q = np.linspace(0.01, 0.5, 1000)
@@ -56,7 +59,11 @@ def abeles(q, layers, scale=1., bkg=0, threads=0):
     mi00 = np.ones((npnts, nlayers + 1), np.complex128)
 
     sld = np.zeros(nlayers + 2, np.complex128)
-    sld[1:] += ((layers[1:, 1] - layers[0, 1]) + 1j * (layers[1:, 2])) * 1.e-6
+
+    # addition of TINY is to ensure the correct branch cut
+    # in the complex sqrt calculation of kn.
+    sld[1:] += ((layers[1:, 1] - layers[0, 1]) +
+                1j * (np.abs(layers[1:, 2]) + TINY)) * 1.e-6
 
     # kn is a 2D array. Rows are Q points, columns are kn in a layer.
     # calculate wavevector in each layer, for each Q point.