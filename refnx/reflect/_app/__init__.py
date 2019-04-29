c01025d4b420a87d5a641549dd157601b2cd3888
Andrew Nelson <andyfaff@gmail.com>
2019-04-29 12:06:19 +1000

    MAINT: gui, enforce decimal point

--- refnx/reflect/_app/__init__.py
+++ refnx/reflect/_app/__init__.py
@@ -2,11 +2,19 @@ import sys
 
 
 def gui():
-    from PyQt5 import QtGui, QtWidgets
+    from PyQt5 import QtGui, QtWidgets, QtCore
     from refnx.reflect._app.view import MotofitMainWindow
     from refnx.reflect._app import resources_rc
 
     app = QtWidgets.QApplication(sys.argv)
+
+    # used to make sure that only decimal point is used for
+    # entering floats (3.1), rejecting commas (3,1).
+    # This reduces confusion for international users
+    lo = QtCore.QLocale(QtCore.QLocale.C)
+    lo.setNumberOptions(QtCore.QLocale.RejectGroupSeparator)
+    QtCore.QLocale.setDefault(lo)
+
     app.setWindowIcon(QtGui.QIcon(':icons/scattering.png'))
     myapp = MotofitMainWindow()
 
