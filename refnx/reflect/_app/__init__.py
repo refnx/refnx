62284ab41cd8e0e8e3da14e400d9ff880c8ea6fc
Andrew Nelson <andyfaff@gmail.com>
2019-06-26 10:49:22 +1000

    MAINT: move high DPI setting

--- refnx/reflect/_app/__init__.py
+++ refnx/reflect/_app/__init__.py
@@ -3,18 +3,21 @@ import sys
 
 def gui():
     from PyQt5 import QtGui, QtWidgets, QtCore
-    from refnx.reflect._app.view import MotofitMainWindow
-    from refnx.reflect._app import resources_rc
-
-    app = QtWidgets.QApplication(sys.argv)
 
     # should enable high resolution on 4k desktops??
     # "https://coad.ca/2017/05/15/
     # one-way-to-deal-with-high-dpi-4k-screens-in-python/"
     if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
-        app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+        QtWidgets.QApplication.setAttribute(
+            QtCore.Qt.AA_EnableHighDpiScaling, True)
     if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
-        app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+        QtWidgets.QApplication.setAttribute(
+            QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+    from refnx.reflect._app.view import MotofitMainWindow
+    from refnx.reflect._app import resources_rc
+
+    app = QtWidgets.QApplication(sys.argv)
 
     # used to make sure that only decimal point is used for
     # entering floats (3.1), rejecting commas (3,1).
