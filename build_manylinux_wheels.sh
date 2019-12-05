b10e793680b2a42e698cda36ac5737d6be5b2d3a
Andrew Nelson <andyfaff@gmail.com>
2019-12-05 12:54:51 +1100

    BLD: update manylinux builder

--- build_manylinux_wheels.sh
+++ build_manylinux_wheels.sh
@@ -3,22 +3,23 @@ set -e -x
 
 # docker run --rm -it -v $(pwd):/io quay.io/pypa/manylinux2010_x86_64 /bin/bash
 # you need to export PLAT so that auditwheel works
-# export PLAT=manylinux2010_x86_64
-# cd io
-# bash build_manylinux_wheels.sh
+ export PLAT=manylinux2010_x86_64
+ cd io
+ bash build_manylinux_wheels.sh
 
 # Compile wheels
-#for PYBIN in /opt/python/cp3[5-9]-cp*/bin; do
-#  "${PYBIN}/pip" install numpy cython
-#  "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-#done
+for PYBIN in /opt/python/cp3[5-9]-cp*/bin; do
+  "${PYBIN}/pip" install numpy cython
+  "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
 
 # Bundle external shared libraries into the wheels
-#for whl in wheelhouse/*.whl; do
-#    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
-#done
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
 
 # Install packages and test
+yum install hdf5-devel
 for PYBIN in /opt/python/cp3[5-9]-cp*/bin; do
   "${PYBIN}/pip" install scipy matplotlib pytest corner uncertainties h5py xlrd periodictable pandas
   "${PYBIN}/pip" install refnx --no-index -f /io/wheelhouse
