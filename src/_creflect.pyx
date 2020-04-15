68e933288efabe5182c54c0979d34a437ad29550
Andrew Nelson <andyfaff@gmail.com>
2020-04-15 16:36:55 +1000

    MAINT: cython noodling about (#431)
    MAINT: cython meddling

--- src/_creflect.pyx
+++ src/_creflect.pyx
@@ -27,9 +27,8 @@ DEALINGS IN THIS SOFTWARE.
 from multiprocessing import cpu_count
 import numpy as np
 
-cimport numpy as np
+cimport numpy as cnp
 cimport cython
-from cython.view cimport array as cvarray
 
 cdef extern from "refcalc.h" nogil:
     void reflect(int numcoefs, const double *coefP, int npoints, double *yP,
@@ -38,7 +37,7 @@ cdef extern from "refcalc.h" nogil:
                    const double *xP, int threads)
 
 DTYPE = np.float64
-ctypedef np.float64_t DTYPE_t
+ctypedef cnp.float64_t DTYPE_t
 
 
 # figure out CPU count
@@ -47,31 +46,38 @@ NCPU = cpu_count()
 
 @cython.boundscheck(False)
 @cython.cdivision(True)
-cpdef np.ndarray abeles(np.ndarray x,
-           np.ndarray[DTYPE_t, ndim=2] w,
-           double scale=1.0, double bkg=0., int threads=-1):
+cpdef cnp.ndarray abeles(cnp.ndarray x,
+                         double[:, :] w,
+                         double scale=1.0,
+                         double bkg=0.,
+                         int threads=-1):
+
     if w.shape[1] != 4 or w.shape[0] < 2:
         raise ValueError("Parameters for _creflect must have shape (>2, 4)")
-    if (w.dtype != np.float64 or x.dtype != np.float64):
+    if x.dtype != np.float64:
         raise ValueError("Parameters for _creflect must be np.float64")
-
-    cdef int nlayers = w.shape[0] - 2
-    cdef int npoints = x.size
-    cdef np.ndarray[DTYPE_t, ndim=1] coefs = np.empty(4 * nlayers + 8,
+    cdef:
+        int nlayers = w.shape[0] - 2
+        int npoints = x.size
+        cnp.ndarray[DTYPE_t, ndim=1] coefs = np.empty(4*nlayers + 8,
                                                       DTYPE)
-    cdef np.ndarray y = np.empty_like(x, DTYPE)
+        double[::1] coefs_view = coefs
+        cnp.ndarray y = np.empty_like(x, DTYPE)
 
-    # we need the abscissae in a contiguous block of memory
-    cdef np.ndarray xtemp = np.ascontiguousarray(x, dtype=DTYPE)
+    if not x.flags['C_CONTIGUOUS']:
+        x = np.ascontiguousarray(x, dtype=DTYPE)
 
-    coefs[0] = nlayers
-    coefs[1] = scale
-    coefs[2:4] = w[0, 1: 3]
-    coefs[4: 6] = w[-1, 1: 3]
-    coefs[6] = bkg
-    coefs[7] = w[-1, 3]
+    coefs_view[0] = nlayers
+    coefs_view[1] = scale
+    coefs_view[2:4] = w[0, 1: 3]
+    coefs_view[4: 6] = w[-1, 1: 3]
+    coefs_view[6] = bkg
+    coefs_view[7] = w[-1, 3]
     if nlayers:
-        coefs[8:] = w.flatten()[4: -4]
+        coefs_view[8::4] = w[1:-1, 0]
+        coefs_view[9::4] = w[1:-1, 1]
+        coefs_view[10::4] = w[1:-1, 2]
+        coefs_view[11::4] = w[1:-1, 3]
 
     if threads == -1:
         threads = NCPU
@@ -81,82 +87,84 @@ cpdef np.ndarray abeles(np.ndarray x,
     with nogil:
         if threads > 1:
             reflectMT(4*nlayers + 8, <const double*>coefs.data, npoints,
-                      <double*>y.data, <const double*>xtemp.data, threads)
+                      <double*>y.data, <const double*>x.data, threads)
         else:
             reflect(4*nlayers + 8, <const double*>coefs.data, npoints,
-                    <double*>y.data, <const double*>xtemp.data)
+                    <double*>y.data, <const double*>x.data)
 
     return y
 
 
-cpdef _contract_by_area(np.ndarray[np.float64_t, ndim=2] slabs, dA=0.5):
+cpdef _contract_by_area(cnp.ndarray[cnp.float64_t, ndim=2] slabs, dA=0.5):
     newslabs = np.copy(slabs)[::-1]
 
-    cdef double [:, :] newslabs_view = newslabs
-    cdef double [:] d = newslabs_view[:, 0]
-    cdef double [:] rho = newslabs_view[:, 1]
-    cdef double [:] irho = newslabs_view[:, 2]
-    cdef double [:] sigma = newslabs[:, 3]
-    cdef double [:] vfsolv = newslabs[:, 4]
+    cdef:
+        double [:, :] newslabs_view = newslabs
+        double [:] d = newslabs_view[:, 0]
+        double [:] rho = newslabs_view[:, 1]
+        double [:] irho = newslabs_view[:, 2]
+        double [:] sigma = newslabs[:, 3]
+        double [:] vfsolv = newslabs[:, 4]
 
-    cdef size_t n = np.size(d, 0)
-    cdef size_t i, newi
-    cdef double dz, rhoarea, irhoarea, vfsolvarea, rholo, rhohi, irholo, irhohi
-    cdef double da = float(dA)
+        size_t n = np.size(d, 0)
+        size_t i, newi
+        double dz, rhoarea, irhoarea, vfsolvarea, rholo, rhohi, irholo, irhohi
+        double da = float(dA)
 
-    i = 1
-    newi = 1 # skip the substrate
+    with nogil:
+        i = 1
+        newi = 1 # skip the substrate
 
-    while i < n:
-        # Get ready for the next layer
-        # Accumulation of the first row happens in the inner loop
-        dz = rhoarea = irhoarea = vfsolvarea = 0.
-        rholo = rhohi = rho[i]
-        irholo = irhohi = irho[i]
+        while i < n:
+            # Get ready for the next layer
+            # Accumulation of the first row happens in the inner loop
+            dz = rhoarea = irhoarea = vfsolvarea = 0.
+            rholo = rhohi = rho[i]
+            irholo = irhohi = irho[i]
 
-        # Accumulate slices into layer
-        while True:
-            # Accumulate next slice
-            dz += d[i]
-            rhoarea += d[i] * rho[i]
-            irhoarea += d[i] * irho[i]
-            vfsolvarea += d[i] * vfsolv[i]
+            # Accumulate slices into layer
+            while True:
+                # Accumulate next slice
+                dz += d[i]
+                rhoarea += d[i] * rho[i]
+                irhoarea += d[i] * irho[i]
+                vfsolvarea += d[i] * vfsolv[i]
 
-            i += 1
-            # If no more slices or sigma != 0, break immediately
-            if i == n or sigma[i - 1] != 0.:
-                break
+                i += 1
+                # If no more slices or sigma != 0, break immediately
+                if i == n or sigma[i - 1] != 0.:
+                    break
 
-            # If next slice won't fit, break
-            if rho[i] < rholo:
-                rholo = rho[i]
-            if rho[i] > rhohi:
-                rhohi = rho[i]
-            if (rhohi - rholo) * (dz + d[i]) > da:
-                break
+                # If next slice won't fit, break
+                if rho[i] < rholo:
+                    rholo = rho[i]
+                if rho[i] > rhohi:
+                    rhohi = rho[i]
+                if (rhohi - rholo) * (dz + d[i]) > da:
+                    break
 
-            if irho[i] < irholo:
-                irholo = irho[i]
-            if irho[i] > irhohi:
-                irhohi = irho[i]
-            if (irhohi - irholo) * (dz + d[i]) > da:
-                break
+                if irho[i] < irholo:
+                    irholo = irho[i]
+                if irho[i] > irhohi:
+                    irhohi = irho[i]
+                if (irhohi - irholo) * (dz + d[i]) > da:
+                    break
 
-        # Save the layer
-        d[newi] = dz
-        if i == n:
-            # printf("contract: adding final sld at %d\n",newi)
-            # Last layer uses surface values
-            rho[newi] = rho[n - 1]
-            irho[newi] = irho[n - 1]
-            vfsolv[newi] = vfsolv[n - 1]
-        else:
-            # Middle layers uses average values
-            rho[newi] = rhoarea / dz
-            irho[newi] = irhoarea / dz
-            sigma[newi] = sigma[i - 1]
-            vfsolv[newi] = vfsolvarea / dz
-        # First layer uses substrate values
-        newi += 1
+            # Save the layer
+            d[newi] = dz
+            if i == n:
+                # printf("contract: adding final sld at %d\n",newi)
+                # Last layer uses surface values
+                rho[newi] = rho[n - 1]
+                irho[newi] = irho[n - 1]
+                vfsolv[newi] = vfsolv[n - 1]
+            else:
+                # Middle layers uses average values
+                rho[newi] = rhoarea / dz
+                irho[newi] = irhoarea / dz
+                sigma[newi] = sigma[i - 1]
+                vfsolv[newi] = vfsolvarea / dz
+            # First layer uses substrate values
+            newi += 1
 
     return newslabs[:newi][::-1]
