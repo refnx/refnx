59696d02bda97b0c1eb51594dc83d1780942da69
Andrew Nelson <andyfaff@gmail.com>
2020-05-12 09:54:32 +1000

    PEP8

--- misc/app/motofit.py
+++ misc/app/motofit.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     # needed for windows using multiprocessing!
     multiprocessing.freeze_support()
 
-    warnings.filterwarnings("ignore", "(?s).*MATPLOTLIBDATA.*", category=UserWarning)
+    warnings.filterwarnings("ignore", "(?s).*MATPLOTLIBDATA.*",
+                            category=UserWarning)
     from refnx.reflect import main
     main()
