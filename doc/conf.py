319f172eaa4b08d37f5166bcdbdd1d0650554a0d
Andrew Nelson <andyfaff@gmail.com>
2020-06-11 19:21:54 +1000

    DOC: jupyter_sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'nbsphinx',
-    'jupyter_sphinx.execute'
+    'jupyter_sphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
