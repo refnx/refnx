06e6fb60289d09e2d26796e244e47f48d9281bf7
Oliver Paull <oliverp@ansto.gov.au>
2021-01-29 16:28:53 +1100

    ADD: SpinChannel and SpinSet classes to handle spin-polarised datasets

--- refnx/reduce/platypusnexus.py
+++ refnx/reduce/platypusnexus.py
@@ -626,6 +626,119 @@ class PolarisedCatalogue(PlatypusCatalogue):
             self.is_magnet = False
         return d
 
+class SpinChannel(Enum):
+    """
+    Describes the spin state of a polarised neutron beam. 
+    """
+    UPUP = (1,1)
+    UPDOWN = (1,0)
+    DOWNUP = (0,1)
+    DOWNDOWN = (0,0)
+
+
+class SpinSet(object):
+    """
+    Describes a set of spin-channels at a given angle. 
+
+    TODO: implement polarisation efficiency correction in this class
+    
+    Parameters
+    ----------
+    list of {str, h5data}
+        list of str, or list of h5py file handles pointing to
+        a set of polarised neutron beam files
+
+    data_folder: {str, Path}
+        Path to the data folder containing the data to be reduced.
+    """
+
+    def __init__(self, dataset, data_folder=None):
+
+        # Currently only Platypus has polarisation elements
+        self.reflect_klass = PlatypusNexus
+
+        self.data_folder = data_folder
+        if len(dataset)> 4:
+            raise ValueError("Too many spin channels!")
+        if len(dataset) < 2:
+            raise ValueError("Too few spin channels! Need at least R++ and R--.")
+            
+        if isinstance(dataset[0], PlatypusNexus):
+            self.beams = self.arrange_spin_channels(dataset)
+        elif type(dataset[0]) is str:
+            fnames = [os.path.join(self.data_folder, dset) for dset in dataset]
+            PLPset = [self.reflect_klass(d) for d in fnames]
+
+            self.beams = self.arrange_spin_channels(*PLPset)
+        else:
+            print('hmm')
+            #self.direct_beams = self.reflect_klass(dataset)
+        
+        self.mm, self.mp, self.pm, self.pp = self.beams
+
+
+    @property
+    def spin_channels(self):
+        return [
+            s.spin_state.value if s is not None else None for s in [
+                self.mm, self.mp, self.pm, self.pp
+            ]
+        ]
+        
+    def arrange_spin_channels(self, *data):
+        """
+        Function that takes a random input of spin channels at 
+        a single angle and returns them arranged according to
+        --, -+, +-, ++ and replaces the spin channel with None
+        if it was not measured.
+        """
+        states = [None]*4
+
+        for a in _not_none(*data):
+            if a.spin_state is SpinChannel.DOWNDOWN:
+                states[0] = a
+            elif a.spin_state is SpinChannel.DOWNUP:
+                states[1] = a
+            elif a.spin_state is SpinChannel.UPDOWN:
+                states[2] = a
+            elif a.spin_state is SpinChannel.UPUP:
+                states[3] = a
+
+        return states
+
+    def process_beams(self, reduction_options=None):
+
+        if reduction_options is None:
+            reduction_options = {
+                "lo_wavelength" : 2.5,
+                "hi_wavelength" : 12.5,
+                "rebin_percent" : 3,
+                }
+
+        for beam in _not_none(self.mm, self.mp, self.pm, self.pp):
+            beam.process(**reduction_options)
+
+    def plot_spectra(self, **kwargs):
+        """
+        Plots the processed spectrums for each spin state in the SpinSet
+
+        Requires matplotlib to be installed
+        """
+        fig, ax = self.mm.plot(**kwargs)
+
+        for spinch in _not_none(self.mp, self.pm, self.pp):
+            fig, ax = spinch.plot(fig=fig, **kwargs)
+
+        fig.legend()
+        return fig, ax 
+
+
+def _not_none(*arrays):
+    """
+    Returns input if input is not not None
+    """
+    return [array for array in arrays if array is not None]
+
 
 class SpinChannel(Enum):
     """
