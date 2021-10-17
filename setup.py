fdf735cd5a4f9790f46328319b0ea3a3eb6ad8a9
Andrew Nelson <andyfaff@gmail.com>
2021-10-18 09:38:48 +1100

    MAINT: try removing openmp code

--- setup.py
+++ setup.py
@@ -389,23 +389,28 @@ def setup_package():
             # export LDFLAGS="$LDFLAGS -L/usr/local/opt/libomp/lib -lomp"
             # export DYLD_LIBRARY_PATH=/usr/local/opt/libomp/lib
 
-            if HAS_OPENMP:
-                # cyreflect extension module
-                _cyreflect = Extension(
-                    name="refnx.reflect._cyreflect",
-                    sources=["src/_cyreflect.pyx"],
-                    include_dirs=[numpy_include],
-                    language="c++",
-                    extra_compile_args=[],
-                    extra_link_args=[]
-                    # libraries=
-                    # extra_compile_args = "...".split(),
-                )
-                openmp_flags = get_openmp_flag(ccompiler)
-                _cyreflect.extra_compile_args += openmp_flags
-                _cyreflect.extra_link_args += openmp_flags
-
-                ext_modules.append(_cyreflect)
+            # if HAS_OPENMP:
+            #     # cyreflect extension module
+            #     _cyreflect = Extension(
+            #         name="refnx.reflect._cyreflect",
+            #         sources=["src/_cyreflect.pyx"],
+            #         include_dirs=[numpy_include],
+            #         language="c++",
+            #         extra_compile_args=[],
+            #         extra_link_args=[]
+            #         # libraries=
+            #         # extra_compile_args = "...".split(),
+            #     )
+            #     openmp_flags = get_openmp_flag(ccompiler)
+            #     _cyreflect.extra_compile_args += openmp_flags
+            #     _cyreflect.extra_link_args += openmp_flags
+            #
+            #     ext_modules.append(_cyreflect)
+            #
+            # # specify min deployment version for macOS
+            # if platform == "darwin":
+            #     for mod in ext_modules:
+            #         mod.extra_compile_args.append("-mmacosx-version-min=10.9")
 
             info["ext_modules"] = cythonize(ext_modules)
             info["zip_safe"] = False
