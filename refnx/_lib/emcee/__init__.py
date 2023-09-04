7b0e39c026ce79699f1a3ee9d70d90a9667e86c0
Andrew Nelson <andyfaff@gmail.com>
2023-09-04 14:34:40 +1000

    Update refnx/_lib/emcee/__init__.py

--- refnx/_lib/emcee/__init__.py
+++ refnx/_lib/emcee/__init__.py
@@ -19,7 +19,7 @@ __license__ = "MIT"
 __description__ = "The Python ensemble sampling toolkit for MCMC"
 
 
-from .emcee_version import __version__  # isort:skip
+__version__ = "703c22dd894a459968717253dc8d9394549d7a9d"
 
 from . import autocorr, backends, moves
 from .ensemble import EnsembleSampler, walkers_independent
