83d0221d41a13c94912ccc7604560786a41ce4c2
Andrew Nelson <andyfaff@gmail.com>
2024-05-01 12:33:35 +1000

    MAINT: fix compile + link flags

--- setup.py
+++ setup.py
@@ -147,11 +147,11 @@ def get_openmp_flag(compiler):
         compiler = compiler.__class__.__name__
 
     if sys.platform == "win32" and ("icc" in compiler or "icl" in compiler):
-        return ["/Qopenmp"]
+        return ["/Qopenmp"], []
     elif sys.platform == "win32":
-        return ["/openmp"]
+        return ["/openmp"], []
     elif sys.platform == "darwin" and ("icc" in compiler or "icl" in compiler):
-        return ["-openmp"]
+        return ["-openmp"], []
     elif sys.platform == "darwin":
         # default for macOS, assuming Apple-clang
         # -fopenmp can't be passed as compile flag when using Apple-clang.
@@ -171,9 +171,9 @@ def get_openmp_flag(compiler):
         # export CXXFLAGS="$CXXFLAGS -I/usr/local/opt/libomp/include"
         # export LDFLAGS="$LDFLAGS -L/usr/local/opt/libomp/lib -lomp"
         # export DYLD_LIBRARY_PATH =/usr/local/opt/libomp/lib
-        return ["-fopenmp"]
+        return ["-fopenmp"], ["-lomp"]
     # Default flag for GCC and clang:
-    return ["-fopenmp"]
+    return ["-fopenmp"], []
 
 
 def check_openmp_support():
@@ -205,8 +205,9 @@ def check_openmp_support():
 
             # Compile, test program
             openmp_flags = get_openmp_flag(ccompiler)
+
             ccompiler.compile(
-                ["test_openmp.c"], output_dir="objects", extra_postargs=openmp_flags
+                ["test_openmp.c"], output_dir="objects", extra_postargs=openmp_flags[0]
             )
 
             # Link test program
@@ -221,7 +222,7 @@ def check_openmp_support():
                 objects,
                 "test_openmp",
                 extra_preargs=extra_preargs,
-                extra_postargs=openmp_flags,
+                extra_postargs=openmp_flags[1],
             )
 
             # Run test program
@@ -389,14 +390,14 @@ def setup_package():
                     include_dirs=[numpy_include],
                     language="c++",
                     extra_compile_args=[],
-                    extra_link_args=["-lomp"],
+                    extra_link_args=[],
                     define_macros=[],
                     extra_objects=refcalc_obj,
                     # extra_compile_args = "...".split(),
                 )
                 openmp_flags = get_openmp_flag(ccompiler)
-                _cyreflect.extra_compile_args += openmp_flags
-                #_cyreflect.extra_link_args += openmp_flags
+                _cyreflect.extra_compile_args += openmp_flags[0]
+                _cyreflect.extra_link_args += openmp_flags[1]
 
                 ext_modules.append(_cyreflect)
 
